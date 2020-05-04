d65dc857d81e42bc16ccbf263bc84ac31a4ae2aa
Vincent Leroy <vincent.leroy@rayference.eu>
2020-05-04 14:24:18 +0200

    Fixes to environment setup script

--- conda_create_env.sh
+++ conda_create_env.sh
@@ -15,17 +15,17 @@ conda activate ${CONDA_ENV_NAME}
 SCRIPT="#!/bin/sh"
 SCRIPT="${SCRIPT}\nexport ERADIATE_DIR=${ROOT}"
 SCRIPT="${SCRIPT}\nexport MITSUBA_DIR=\"\${ERADIATE_DIR}\"/ext/mistuba2"
-SCRIPT="${SCRIPT}\nexport PYTHONPATH=\"\${ERADIATE_DIR}/dist/python:\$PYTHONPATH\""
-SCRIPT="${SCRIPT}\nexport PATH=\"\${ERADIATE_DIR}/dist:\$PATH\""
+SCRIPT="${SCRIPT}\nexport PYTHONPATH=\"\${ERADIATE_DIR}/build/dist/python:\$PYTHONPATH\""
+SCRIPT="${SCRIPT}\nexport PATH=\"\${ERADIATE_DIR}/build/dist:\$PATH\""
 mkdir -p ${CONDA_PREFIX}/etc/conda/activate.d
 echo -e ${SCRIPT} > ${CONDA_PREFIX}/etc/conda/activate.d/env_vars.sh
 
 SCRIPT="#!/bin/sh"
 # Clean up PATH
-SCRIPT="${SCRIPT}\nmatch=\"\${ERADIATE_DIR}/dist:\""
+SCRIPT="${SCRIPT}\nmatch=\"\${ERADIATE_DIR}/build/dist:\""
 SCRIPT="${SCRIPT}\nexport PATH=\${PATH//\$match/}"
 # Clean up PYTHONPATH
-SCRIPT="${SCRIPT}\nmatch=\"\${ERADIATE_DIR}/dist/python:\""
+SCRIPT="${SCRIPT}\nmatch=\"\${ERADIATE_DIR}/build/dist/python:\""
 SCRIPT="${SCRIPT}\nexport PYTHONPATH=\"\${PYTHONPATH//\$match/}\""
 # Remove other environment variables
 SCRIPT="${SCRIPT}\nunset ERADIATE_DIR"
@@ -39,3 +39,4 @@ echo
 echo "Activate conda env ${CONDA_ENV_NAME} using the following command:"
 echo
 echo "    conda activate ${CONDA_ENV_NAME}"
+echo
