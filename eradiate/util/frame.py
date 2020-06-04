af584d5351b34ea6e529281370dda0dd06fc6bee
Vincent Leroy <vincent.leroy@rayference.eu>
2020-06-04 12:36:28 +0200

    Fixed docstring in frame.py

--- eradiate/util/frame.py
+++ eradiate/util/frame.py
@@ -6,13 +6,15 @@ import numpy as np
 def cos_angle_to_direction(cos_theta, phi):
     r"""Convert a zenith cosine and azimuth angle pair to a direction.
 
-    Parameters:
-    ``theta`` (float): Zenith angle cosine [dimensionless].
+    Parameter ``theta`` (float):
+        Zenith angle cosine [dimensionless].
         Convention: 1 corresponds to zenith, -1 corresponds to nadir.
-    ``phi`` (float): Azimuth angle [radian].
+
+    Parameter ``phi`` (float):
+        Azimuth angle [radian].
         Convention: :math:`2 \pi` corresponds to the X axis.
 
-    Returns np.ndarray: 
+    Returns → array:
         Direction corresponding to the angular parameters.
     """
     sin_theta = np.sqrt(1.0 - cos_theta * cos_theta)
@@ -23,13 +25,15 @@ def cos_angle_to_direction(cos_theta, phi):
 def angles_to_direction(theta, phi):
     r"""Convert a zenith and azimuth angle pair to a direction.
 
-    Parameters:
-    ``theta``(float): Zenith angle [radian].
+    Parameter ``theta`` (float):
+        Zenith angle [radian].
         Convention: 0 corresponds to zenith, :math:`\pi` corresponds to nadir.
-    ``phi``(float): Azimuth angle [radian].
+
+    Parameter ``phi`` (float):
+        Azimuth angle [radian].
         Convention: :math:`2 \pi` corresponds to the X axis.
 
-    Returns np.ndarray:
+    Returns → array:
         Direction corresponding to the angular parameters.
     """
     return cos_angle_to_direction(np.cos(theta), phi)
@@ -39,10 +43,11 @@ def direction_to_angles(wi):
     """Converts a cartesian 3-vector to a pair of theta and phi values
     in spherical coordinates
 
-    Parameter ``wi`` (array): 3-vector designating a direction in cartesian coordinates
+    Parameter ``wi`` (array): 
+        3-vector designating a direction in cartesian coordinates
 
-    Returns array:
-        Zenith and azimuth angles in radians, where zenith=0 corresponds to 
+    Returns → list[float]:
+        Zenith and azimuth angles in radians, where zenith=0 corresponds to
         +z direction
     """
     wi = wi / np.linalg.norm(wi)
