1ea6f97363871c83a781b12403b43ea89a3b5a0c
Vincent Leroy <vincent.leroy@rayference.eu>
2020-06-26 11:18:19 +0200

    Added eradiate.__version__

--- setup.py
+++ setup.py
@@ -1,8 +1,27 @@
+import codecs
+import os.path
+
 from setuptools import setup, find_packages
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name="eradiate",
-    version="0.0.1",
+    version=get_version("eradiate/__init__.py"),
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     install_requires=[],
