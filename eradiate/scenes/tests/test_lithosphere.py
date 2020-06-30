a928f4f458504beba4caa66eca68605035eccf5f
Vincent Leroy <vincent.leroy@rayference.eu>
2020-06-30 20:34:11 +0200

    Fixed broken test

--- eradiate/scenes/tests/test_lithosphere.py
+++ eradiate/scenes/tests/test_lithosphere.py
@@ -2,12 +2,13 @@ from eradiate.scenes import SceneDict
 from eradiate.scenes.lithosphere import Lambertian, RPV
 
 
-
 def test_lambertian(variant_scalar_mono):
+    from eradiate.kernel.core.xml import load_dict
+
     # Default constructor
     ls = Lambertian()
 
-    # Check if produced scene can be instanitated
+    # Check if produced scene can be instantiated
     scene_dict = SceneDict.empty()
     scene_dict.add(ls)
     assert scene_dict.load() is not None
@@ -37,4 +38,3 @@ def test_rpv(variant_scalar_mono):
     # Check if produced scene can be instantiated
     assert SceneDict.empty().add(ls).load() is not None
     assert load_dict(scene_dict) is not None
-
