d0478325ac6f992d7bdd6d27a7d939d8315d92d2
Yvan Nollet <yvan.nollet@rayference.eu>
2020-10-07 17:46:59 +0200

    Resolve "`blackbody_sun` solar irradiance spectrum dataset"

--- eradiate/data/__init__.py
+++ eradiate/data/__init__.py
@@ -31,6 +31,7 @@ def get(path):
 
 
 SOLAR_IRRADIANCE_SPECTRA = {
+    "blackbody_sun": "spectra/blackbody_sun.nc",
     "thuillier_2003": "spectra/thuillier_2003.nc",
 }
 """This dictionary provides access to solar irradiance data sets shipped with 
@@ -60,6 +61,9 @@ identifiers.
    * - Key
      - Reference
      - Spectral range [nm]
+   * - ``blackbody_sun``
+     - :cite:`Liou2002IntroductionAtmosphericRadiation`
+     - [280, 2400]
    * - ``thuillier_2003``
      - :cite:`Thuillier2003SolarSpectralIrradiance`
      - [200, 2397]
