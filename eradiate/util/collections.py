6c66a03ec9c31dd0dfdc2cd092a355b162ad4f50
Vincent Leroy <vincent.leroy@rayference.eu>
2021-01-07 13:27:07 +0100

    Added function to check if value is a 3-vector

--- eradiate/util/collections.py
+++ eradiate/util/collections.py
@@ -1,8 +1,31 @@
 """Specialised container datatypes providing alternatives to Python’s general
 purpose built-in containers, dict, list, set, and tuple."""
 
+from collections.abc import Sequence
+from numbers import Number
+
+import numpy
+import pint
 from dpath import util as dpu
 from dpath.exceptions import PathNotFound
+
+
+def is_vector3(value):
+    """Returns ``True`` if a value can be interpreted as a 3-vector."""
+
+    if isinstance(value, pint.Quantity):
+        return is_vector3(value.magnitude)
+
+    # @formatter:off
+    return (
+        (
+            isinstance(value, numpy.ndarray) or
+            (isinstance(value, Sequence) and not isinstance(value, str))
+        )
+        and len(value) == 3
+        and all(map(lambda x: isinstance(x, Number), value))
+    )
+    # @formatter:on
 
 
 def onedict_value(d):
--- eradiate/util/tests/test_collections.py
+++ eradiate/util/tests/test_collections.py
@@ -1,7 +1,27 @@
+import numpy as np
 import pytest
 
-from eradiate.util.collections import ndict
+from eradiate.util.collections import is_vector3, ndict
 from eradiate.util.units import ureg
+
+vector3_test_data = [
+    ("aaa", False),
+    ([0, 1], False),
+    ([0, 2], False),
+    ([0, 1, 2], True),
+    ([0., 1, 2], True),
+    ([0, 1, "2"], False),
+    (np.array([0, 1, 2]), True),
+    (np.array([0, 1]), False),
+    (np.array(["0", 1, 2]), False),
+    (ureg.Quantity([0, 1, 2], "m"), True)
+]
+
+
+@pytest.mark.parametrize("value, expected", vector3_test_data)
+def test_is_vector3(value, expected):
+    result = is_vector3(value)
+    assert result == expected
 
 
 def test_ndict_construct():
