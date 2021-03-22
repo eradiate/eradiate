49879feaeb84f8f283a4f8dca0131e0f65accc77
Nicolas Misk <nicolas.misk@rayference.eu>
2021-03-22 18:07:41 +0100

    Update dirty template

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import setuptools
 setuptools.setup(
     version_config={
         "dev_template": "{tag}",
+        "dirty_template": "{tag}",
     },
     setup_requires=['setuptools-git-versioning'],
 )
