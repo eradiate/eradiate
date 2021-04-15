2a1473df56ae542a8ea3abf18e27e2418967e89d
Vincent Leroy <vincent.leroy@rayference.eu>
2021-04-15 14:25:41 +0200

    measure: Fix missing import

--- eradiate/scenes/measure/__init__.py
+++ eradiate/scenes/measure/__init__.py
@@ -2,6 +2,7 @@ from ._core import Measure, MeasureFactory
 from ._distant import DistantMeasure
 from ._perspective import PerspectiveCameraMeasure
 from ._radiancemeter import RadiancemeterMeasure
+from ._radiancemeterarray import RadiancemeterArrayMeasure
 
 __all__ = [
     "Measure",
