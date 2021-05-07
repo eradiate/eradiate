34b6cf3ee1e32d3c0bed34392a199eaea1bd1e2f
Vincent Leroy <vincent.leroy@rayference.eu>
2021-05-07 12:09:18 +0200

    scenes.measure: Add MeasureSpectralConfig

--- eradiate/scenes/measure/__init__.py
+++ eradiate/scenes/measure/__init__.py
@@ -1,4 +1,4 @@
-from ._core import Measure, MeasureFactory
+from ._core import Measure, MeasureFactory, MeasureSpectralConfig
 from ._distant import DistantMeasure
 from ._perspective import PerspectiveCameraMeasure
 from ._radiancemeter import RadiancemeterMeasure
@@ -6,6 +6,7 @@ from ._radiancemeterarray import RadiancemeterArrayMeasure
 
 __all__ = [
     "Measure",
+    "MeasureSpectralConfig",
     "MeasureFactory",
     "DistantMeasure",
     "PerspectiveCameraMeasure",
