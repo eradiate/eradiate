f2c27c81a306cdb787959286616d42e44df2b4f6
Vincent Leroy <vincent.leroy@rayference.eu>
2021-05-13 19:56:16 +0200

    warp: Use ensure_array()

--- eradiate/warp.py
+++ eradiate/warp.py
@@ -1,8 +1,10 @@
 import numpy as np
 from numpy.typing import ArrayLike
 
+from eradiate._util import ensure_array
 
-def square_to_uniform_disk_concentric(sample: ArrayLike):
+
+def square_to_uniform_disk_concentric(sample: ArrayLike) -> ArrayLike:
     """
     Low-distortion concentric square to disk mapping.
 
@@ -12,8 +14,7 @@ def square_to_uniform_disk_concentric(sample: ArrayLike):
     """
     # Matches Mitsuba implementation
 
-    if not isinstance(sample, np.ndarray):
-        sample = np.array(sample)
+    sample = ensure_array(sample)
     if sample.ndim < 2:
         sample = sample.reshape((sample.size // 2, 2))
     if sample.ndim > 2 or sample.shape[1] != 2:
@@ -37,14 +38,13 @@ def square_to_uniform_disk_concentric(sample: ArrayLike):
     return np.vstack((r * c, r * s)).T
 
 
-def square_to_uniform_hemisphere(sample: ArrayLike):
+def square_to_uniform_hemisphere(sample: ArrayLike) -> ArrayLike:
     """
     Uniformly sample a vector on the unit hemisphere with respect to solid angles.
     """
     # Matches Mitsuba implementation
 
-    if not isinstance(sample, np.ndarray):
-        sample = np.array(sample)
+    sample = ensure_array(sample)
     if sample.ndim < 2:
         sample = sample.reshape((sample.size // 2, 2))
     if sample.ndim > 2 or sample.shape[1] != 2:
@@ -56,31 +56,31 @@ def square_to_uniform_hemisphere(sample: ArrayLike):
     return np.vstack((p[..., 0], p[..., 1], z)).T
 
 
-def uniform_hemisphere_to_square(v: ArrayLike):
+def uniform_hemisphere_to_square(v: ArrayLike) -> ArrayLike:
     """
     Inverse of the mapping square_to_uniform_hemisphere.
     """
     # Matches Mitsuba implementation
 
-    if not isinstance(v, np.ndarray):
-        v = np.array(v)
+    v = ensure_array(v)
     if v.ndim < 2:
         v = v.reshape((v.size // 3, 3))
     if v.ndim > 2 or v.shape[1] != 3:
         raise ValueError(f"array must be of shape (N, 3), got {v.shape}")
 
     p = v[..., 0:2]
-    return uniform_disk_to_square_concentric(p / np.sqrt(v[..., 2] + 1.0))
+    return uniform_disk_to_square_concentric(
+        p / np.sqrt(v[..., 2] + 1.0).reshape((len(p), 1))
+    )
 
 
-def uniform_disk_to_square_concentric(p: ArrayLike):
+def uniform_disk_to_square_concentric(p: ArrayLike) -> ArrayLike:
     """
     Inverse of the mapping square_to_uniform_disk_concentric.
     """
     # Matches Mitsuba implementation
 
-    if not isinstance(p, np.ndarray):
-        p = np.array(p)
+    p = ensure_array(p)
     if p.ndim < 2:
         p = p.reshape((p.size // 2, 2))
     if p.ndim > 2 or p.shape[1] != 2:
