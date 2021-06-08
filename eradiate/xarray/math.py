8bc3ff3c7708a1e009764beed03169089edc0eb7
Yvan Nollet <yvan.nollet@rayference.eu>
2021-06-08 17:36:03 +0200

    Added xarray.math module

---
+++ eradiate/xarray/math.py
@@ -0,0 +1,20 @@
+from typing import List, Optional
+
+import xarray as xr
+
+
+def weighted_mean(
+    data_arrays: List[xr.DataArray], weights: List[xr.DataArray]
+) -> xr.DataArray:
+    """Compute the weighted mean.
+
+    Parameter ``data_arrays`` (list of :class:`~xarray.DataArray`):
+        Data arrays of which to compute the weighted mean.
+
+    Parameter ``weights`` (list of :class:`~xarray.DataArray`):
+        Weights data.
+
+    Returns → :class:`~xarray.DataArray`:
+        Weighted mean data array.
+    """
+    return sum([da * w for da, w in zip(data_arrays, weights)]) / sum(weights)
