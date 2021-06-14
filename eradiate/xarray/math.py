bbd6441f31d82fc9f58d49ab84c34e4e97fd6d1c
Yvan Nollet <yvan.nollet@rayference.eu>
2021-06-14 14:21:18 +0200

    Added ratios and tests

--- eradiate/xarray/math.py
+++ eradiate/xarray/math.py
@@ -1,5 +1,6 @@
 from typing import List
 
+import numpy as np
 import xarray as xr
 
 
@@ -18,3 +19,30 @@ def weighted_mean(
         Weighted mean data array.
     """
     return sum([da * w for da, w in zip(data_arrays, weights)]) / sum(weights)
+
+
+def ratios(data_arrays: List[xr.DataArray]) -> xr.DataArray:
+    """Compute the ratios of data arrays.
+
+    Provided all data arrays give the value of a quantity :math:`q`, this
+    function computes the ratios :math:`r_i` defined by:
+
+    .. math::
+       r_i = \\frac{q_i}{\\sum_{i}q_i}
+
+    Parameter ``data_arrays`` (list of :class:`~xarray.DataArray`):
+        Data arrays of which to compute the ratios.
+
+    Returns → :class:`~xarray.DataArray`:
+        Ratios.
+    """
+
+    da_sum = sum([da for da in data_arrays])
+
+    ratios = []
+    for i, da in enumerate(data_arrays):
+        ratio = da / da_sum
+        tmp = xr.DataArray(ratio.data[np.newaxis, :], dims=["i"] + list(ratio.dims))
+        ratios.append(tmp.assign_coords(ratio.coords).assign_coords({"i": [i]}))
+
+    return xr.concat(ratios, dim="i")
