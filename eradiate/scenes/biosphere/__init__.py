53e0aa6b50da0169227c6ff9c4e9324e09f13b1b
Sebastian Schunke <sebastian.schunke@rayference.eu>
2021-06-18 16:51:04 +0200

    biosphere._distant: Split into multiple modules with more specialised scopes

--- eradiate/scenes/biosphere/__init__.py
+++ eradiate/scenes/biosphere/__init__.py
@@ -1,11 +1,8 @@
+from ._canopy_element import CanopyElement
 from ._core import BiosphereFactory, Canopy
-from ._discrete import (
-    AbstractTree,
-    CanopyElement,
-    DiscreteCanopy,
-    InstancedCanopyElement,
-    LeafCloud,
-)
+from ._discrete import DiscreteCanopy, InstancedCanopyElement
+from ._leaf_cloud import LeafCloud
+from ._tree import AbstractTree
 
 __all__ = [
     "BiosphereFactory",
