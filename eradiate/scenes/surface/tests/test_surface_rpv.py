b1f5e42a77a9530b5e69d9531c62925c620733f3
Vincent Leroy <vincent.leroy@rayference.eu>
2021-06-18 16:01:31 +0200

    RPVSurface: Update defaults, match params with Scientific Handbook, accept spectra

--- eradiate/scenes/surface/tests/test_surface_rpv.py
+++ eradiate/scenes/surface/tests/test_surface_rpv.py
@@ -10,15 +10,27 @@ def test_rpv(mode_mono):
     ctx = KernelDictContext()
 
     # Default constructor
-    ls = RPVSurface()
+    surface = RPVSurface()
 
     # Check if produced scene can be instantiated
-    kernel_dict = KernelDict.new(ls, ctx=ctx)
+    kernel_dict = KernelDict.new(surface, ctx=ctx)
     assert kernel_dict.load() is not None
 
-    # Constructor with arguments
-    ls = RPVSurface(width=ureg.Quantity(1000.0, "km"), rho_0=0.3, k=1.4, ttheta=-0.23)
-    assert np.allclose(ls.width, ureg.Quantity(1e6, ureg.m))
+    # Construct from floats
+    surface = RPVSurface(width=ureg.Quantity(1000.0, "km"), rho_0=0.3, k=1.4, g=-0.23)
+    assert np.allclose(surface.width, ureg.Quantity(1e6, ureg.m))
+
+    # Construct from mixed spectrum types
+    surface = RPVSurface(
+        width=ureg.Quantity(1000.0, "km"),
+        rho_0=0.3,
+        k={"type": "uniform", "value": 0.3},
+        g={
+            "type": "interpolated",
+            "wavelengths": [300.0, 800.0],
+            "values": [-0.23, 0.23],
+        },
+    )
 
     # Check if produced scene can be instantiated
-    assert KernelDict.new(ls, ctx=ctx).load() is not None
+    assert KernelDict.new(surface, ctx=ctx).load() is not None
