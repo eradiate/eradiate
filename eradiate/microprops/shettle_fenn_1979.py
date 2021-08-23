f0f1f0675a5d691662e4f2fd1c9a1b567f3d061a
Yvan Nollet <yvan.nollet@rayference.eu>
2021-08-23 18:18:00 +0200

    Work in progress

--- eradiate/microprops/shettle_fenn_1979.py
+++ eradiate/microprops/shettle_fenn_1979.py
@@ -1,40 +1,40 @@
 """
 Aerosols models according to :cite:`Shettle1979ModelsAerosolsLower`.
 """
-from typing import Callable
-
 import numpy as np
+from scipy.stats import lognorm
 
 from ..units import unit_registry as ureg
 
 
+def lognorm(r: np.ndarray, ri: float, si: float, ni: float = 1.0):
+    """
+    Compute the log-normal distribution as in equation (1) of
+    :cite:`Shettle1979ModelsAerosolsLower`.
+    """
+    return (ni / (np.log(10) * r * si * np.sqrt(2 * np.pi))) * np.exp(
+        -np.square(np.log10(r) - np.log10(ri)) / (2 * np.square(si))
+    )
+
+
 @ureg.wraps(
     ret=None,
-    args=("micrometer", "cm^-3 * micrometer^-1", "dimensionless"),
+    args=(
+        "micrometer",
+        "micrometer",
+        "micrometer",
+        "cm^-3",
+        "cm^-3",
+        "dimensionless",
+        "dimensionless",
+    ),
     strict=False,
 )
-def size_distribution(mr: np.ndarray, nd: np.ndarray, std: np.ndarray) -> Callable:
+def size_distribution(
+    r: np.ndarray, r1: float, r2: float, n1: float, n2: float, s1: float, s2: float
+) -> np.ndarray:
     """
-    Return a function that evaluate the particle cumulative number density per
-    unit of radius.
-
-    This function implements the equation (1) in
+    Compute the aerosol size distribution according to equation (1) of
     :cite:`Shettle1979ModelsAerosolsLower`.
-
-    Parameter ``mr`` (:class:`~numpy.ndarray`):
-        Mode radius [micrometer].
-
-    Parameter ``nd`` (:class:`~numpy.ndarray`):
-        Number density per unit of radius [cm^-3 * micrometer^-1].
-
-    Parameter ``std`` (:class:`~numpy.ndarray`):
-        Standard deviation [dimensionless].
-
-    Returns → Callable:
-        A function :class:`~numpy.ndarray` → :class:`~numpy.ndarray` that
-        evaluate the particle cumulative number density per unit of radius.
     """
-
-    return lambda r: (nd / np.log(10) * r * std * np.sqrt(2)) * np.exp(
-        -(np.square(np.log10(r) - np.log10(mr))) / (2 * np.square(std))
-    )
+    return lognorm(r=r, ri=r1, si=s1, ni=n1) + lognorm(r=r, ri=r2, si=s2, ni=n2)
