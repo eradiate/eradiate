0144346c454739a5f1af8adb38854de57358b0a1
Vincent Leroy <vincent.leroy@rayference.eu>
2021-10-03 20:47:27 +0200

    experiments: Add RamiExperiment
    This commit transitions the RamiSolverApp line to the Experiment class
    hierarchy. Tests are slightly improved with respect to the RamiSolverApp
    line.

--- eradiate/experiments/__init__.py
+++ eradiate/experiments/__init__.py
@@ -1,9 +1,11 @@
 from ._core import EarthObservationExperiment, Experiment, mitsuba_run
 from ._onedim import OneDimExperiment
+from ._rami import RamiExperiment
 
 __all__ = [
     "mitsuba_run",
     "EarthObservationExperiment",
     "Experiment",
     "OneDimExperiment",
+    "RamiExperiment",
 ]
