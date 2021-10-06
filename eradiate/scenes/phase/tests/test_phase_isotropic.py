5b4ac01b70999a1757e729e42e71f09eff944455
Vincent Leroy <vincent.leroy@rayference.eu>
2021-10-06 19:11:40 +0200

    phase: Add IsotropicPhaseFunction

---
+++ eradiate/scenes/phase/tests/test_phase_isotropic.py
@@ -0,0 +1,14 @@
+from eradiate.contexts import KernelDictContext
+from eradiate.scenes.core import KernelDict
+from eradiate.scenes.phase import IsotropicPhaseFunction
+
+
+def test_phase_isotropic(modes_all):
+    ctx = KernelDictContext()
+
+    # Default constructor
+    phase = IsotropicPhaseFunction()
+
+    # Check if produced kernel dict can be instantiated
+    kernel_dict = KernelDict.from_elements(phase, ctx=ctx)
+    assert kernel_dict.load() is not None
