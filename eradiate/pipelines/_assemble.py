c21e43498c75ee66a4566add469068c68f11c613
Vincent Leroy <vincent.leroy@rayference.eu>
2021-10-26 12:17:03 +0200

    pipelines: Add AddIllumination

--- eradiate/pipelines/_assemble.py
+++ eradiate/pipelines/_assemble.py
@@ -4,17 +4,21 @@ import warnings
 import attr
 import numpy as np
 import pint
-import pinttr
 import xarray as xr
+
+import eradiate
 
 from ._core import PipelineStep
 from .. import unit_registry as ureg
+from .._mode import ModeFlags
 from ..attrs import documented, parse_docs
+from ..exceptions import UnsupportedModeError
 from ..frame import angles_in_hplane
-from ..scenes.illumination import DirectionalIllumination
+from ..scenes.illumination import ConstantIllumination, DirectionalIllumination
 from ..scenes.measure import Measure
-from ..units import symbol
-from ..units import unit_context_config as ucc
+from ..scenes.spectra import Spectrum
+from ..units import symbol, to_quantity
+from ..units import unit_context_kernel as uck
 
 
 @parse_docs
@@ -23,28 +27,134 @@ class AddIllumination(PipelineStep):
     """
     Add illumination data.
 
-    This post-processing pipeline step adds Sun angle coordinates and
-    illumination data:
+    This post-processing pipeline step adds illumination data:
 
-    * the ``sza`` (Sun zenith angle) and ``saa`` (Sun azimuth angle) scalar
-      coordinates are defined and attached to all existing variables;
-    * an ``illumination`` data variable is created, with dimensions ``sza`` and
-      ``vaa``.
+    * if `illumination` is a :class:`.DirectionalIllumination` instance, then
+      an ``irradiance`` data variable is created, with dimensions ``sza`` and
+      ``vaa``, which holds the incoming top-of-scene flux with respect to a
+      horizontal surface;
+    * if `illumination` is a :class:`.ConstantIllumination` instance, then
+      the ``irradiance`` data variable has no coordinate.
     """
 
-    illumination: DirectionalIllumination = documented(
+    illumination: t.Union[DirectionalIllumination, ConstantIllumination] = documented(
         attr.ib(
-            validator=attr.validators.instance_of(DirectionalIllumination),
+            validator=attr.validators.instance_of(
+                (DirectionalIllumination, ConstantIllumination)
+            ),
             repr=lambda self: f"{self.__class__.__name__}(id='{self.id}', ...)",
         ),
-        doc="A :class:`.DirectionalIllumination` instance from which the "
-        "illumination data originates.",
-        type=":class:`.DirectionalIllumination`",
+        doc="An :class:`.Illumination` instance from which the illumination "
+        "data originates.",
+        type=":class:`.DirectionalIllumination` or :class:`.ConstantIllumination`",
+    )
+
+    measure: Measure = documented(
+        attr.ib(
+            validator=attr.validators.instance_of(Measure),
+            repr=lambda self: f"{self.__class__.__name__}(id='{self.id}', ...)",
+        ),
+        doc="A :class:`.Measure` instance from the data originates.",
+        type=":class:`.Measure`",
     )
 
     def transform(self, x: t.Any) -> t.Any:
-        # TODO
-        raise NotImplementedError
+        k_irradiance_units = uck.get("irradiance")
+        illumination = self.illumination
+        measure = self.measure
+        result = x.copy()
+
+        # Collect spectral coordinate values for verification purposes
+        wavelengths_dataset = to_quantity(x.w)
+
+        def eval_illumination_spectrum(
+            field_name: str, k_units: pint.Unit
+        ) -> pint.Quantity:
+            # Local helper function to help with illumination spectrum evaluation
+
+            spectrum: Spectrum = getattr(illumination, field_name)
+
+            if eradiate.mode().has_flags(ModeFlags.ANY_MONO):
+                # Very important: sort spectral coordinate
+                wavelengths = np.sort(measure.spectral_cfg.wavelengths)
+                assert np.allclose(wavelengths, wavelengths_dataset)
+                return spectrum.eval_mono(wavelengths).m_as(k_units)
+
+            elif eradiate.mode().has_flags(ModeFlags.ANY_CKD):
+                # Collect bins and wavelengths, evaluate spectrum
+                bins = measure.spectral_cfg.bins
+                wavelengths = [bin.wcenter.m_as(ureg.nm) for bin in bins] * ureg.nm
+
+                # Note: This line assumes that eval_ckd() returns a constant
+                # value over the entire bin
+                result = spectrum.eval_ckd(*(bin.bindexes[0] for bin in bins)).m_as(
+                    k_units
+                )
+
+                # Reorder data by ascending wavelengths
+                indices = wavelengths.argsort()
+                wavelengths = wavelengths[indices]
+                assert np.allclose(wavelengths, wavelengths_dataset)
+                result = result[indices]
+
+                return result
+
+            else:
+                raise UnsupportedModeError(supported=("monochromatic", "ckd"))
+
+        if isinstance(illumination, DirectionalIllumination):
+            # Collect illumination angular data
+            saa = illumination.azimuth.m_as(ureg.deg)
+            sza = illumination.zenith.m_as(ureg.deg)
+            cos_sza = np.cos(np.deg2rad(sza))
+
+            # Add angular dimensions
+            result = result.expand_dims({"sza": [sza], "saa": [saa]}, axis=(0, 1))
+            result.coords["sza"].attrs = {
+                "standard_name": "solar_zenith_angle",
+                "long_name": "solar zenith angle",
+                "units": symbol("deg"),
+            }
+            result.coords["saa"].attrs = {
+                "standard_name": "solar_azimuth_angle",
+                "long_name": "solar azimuth angle",
+                "units": symbol("deg"),
+            }
+
+            # Collect illumination spectral data
+            irradiances = eval_illumination_spectrum("irradiance", k_irradiance_units)
+
+            # Add irradiance variable
+            result["irradiance"] = (
+                ("sza", "saa", "w"),
+                (irradiances * cos_sza).reshape((1, 1, len(irradiances))),
+            )
+
+        elif isinstance(illumination, ConstantIllumination):
+            # Collect illumination spectral data
+            k_radiance_units = uck.get("radiance")
+            radiances = eval_illumination_spectrum("radiance", k_radiance_units)
+
+            # Add irradiance variable
+            result["irradiance"] = (
+                ("w",),
+                np.pi * radiances.reshape((len(radiances),)),
+            )
+
+        else:
+            raise TypeError(
+                "keyword argument 'illumination' must be one of "
+                "(DirectionalIllumination, ConstantIllumination), got a "
+                f"{illumination.__class__.__name__}"
+            )
+
+        result["irradiance"].attrs = {
+            "standard_name": "horizontal_solar_irradiance_per_unit_wavelength",
+            "long_name": "horizontal spectral irradiance",
+            "units": symbol(k_irradiance_units),
+        }
+
+        return result
 
 
 @attr.s
