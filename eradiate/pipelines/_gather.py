pipelines.Gather: Fix wrong dimensions for spp, update test fixtures

eradiate/pipelines/_gather.py
@@ -2,6 +2,7 @@ import re
 import typing as t
 
 import attr
+import numpy as np
 import xarray as xr
 from pinttr.util import always_iterable
 
@@ -154,19 +155,25 @@ class Gather(PipelineStep):
                 # Add spp dimension even though sample count split did not
                 # produce any extra sensor
                 if "spp" not in sensor_dims:
-                    spectral_coords["spp_index"] = [0]
+                    sensor_coords["spp_index"] = [0]
 
                 # Add spectral and sensor dimensions to img array
-                ds["img"] = ds.img.expand_dims(dim={**spectral_coords, **sensor_coords})
+                all_coords = {**spectral_coords, **sensor_coords}
+                ds["img"] = ds.img.expand_dims(dim=all_coords)
 
                 # Package spp in a data array
-                ds["spp"] = ("spp_index", [spp])
+                all_dims = list(all_coords.keys())
+                ds["spp"] = (all_dims, np.reshape(spp, [1 for _ in all_dims]))
 
                 sensor_datasets.append(ds)
 
         # Combine all the data
         with xr.set_options(keep_attrs=True):
             result = xr.merge(sensor_datasets)
+
+        # Drop "channel" dimension when using a mono variant
+        if eradiate.mode().has_flags(ModeFlags.MTS_MONO):
+            result = result.squeeze("channel", drop=True)
 
         # Apply metadata to new dimensions
         for sensor_dim in sensor_dims:

eradiate/pipelines/tests/test_pipelines_gather.py
@@ -3,92 +3,214 @@ import numpy as np
 from eradiate.pipelines._gather import Gather
 
 
-def test_pipeline_step_gather_mono_ms_spp(results_mono_ms_spp):
-    values = results_mono_ms_spp[0]
-
-    # Configure this pipeline step according to the used sensor
-    step = Gather(sensor_dims=("ms", "spp"))
-    result = step.transform(values)
-
-    # Check that all dimensions are here as expected
-    assert result.dims == {
-        "w": 1,
-        "ms_index": 2,
-        "spp_index": 3,
-        "y_index": 1,
-        "x_index": 1,
-        "channel": 1,
-    }
-    assert set(result.keys()) == {"img", "spp"}
-
-    # Check radiance and sample count values
-    assert np.allclose(2.0 / np.pi, result.img.values)
-    assert np.all([100, 100, 50] == result.spp.values)
-
-
-def test_pipeline_step_assemble_ckd_ms_spp(results_ckd_ms_spp):
-    values = results_ckd_ms_spp[0]
-
-    # Configure this pipeline step according to the used sensor
-    step = Gather(sensor_dims=("ms", "spp"))
-    result = step.transform(values)
-
-    # Check that all dimensions are here as expected
-    assert result.dims == {
-        "bin": 1,
-        "index": 16,
-        "ms_index": 2,
-        "spp_index": 3,
-        "y_index": 1,
-        "x_index": 1,
-        "channel": 1,
-    }
-    assert set(result.keys()) == {"img", "spp"}
-
-    # Check radiance and sample count values
-    assert np.allclose(2.0 / np.pi, result.img.values)
-    assert np.all([100, 100, 50] == result.spp.values)
-
-
-def test_pipeline_step_assemble_mono_spp(results_mono_spp):
-    values = results_mono_spp[0]
-
-    # Configure this pipeline step according to the used sensor
-    step = Gather(sensor_dims=("spp",))
-    result = step.transform(values)
-
-    # Check that all dimensions are here as expected
-    assert result.dims == {
-        "w": 1,
-        "spp_index": 3,
-        "y_index": 32,
-        "x_index": 32,
-        "channel": 1,
-    }
-    assert set(result.keys()) == {"img", "spp"}
-
-    # Check radiance and sample count values
-    assert np.allclose(2.0 / np.pi, result.img.values)
-    assert np.all([100, 100, 50] == result.spp.values)
-
-
 def test_pipeline_step_assemble_mono(results_mono):
+    """
+    Most basic test case for :class:`.Gather`:
+
+    * mono mode;
+    * single-sensor measure;
+    * no sample count split.
+    """
     values = results_mono[0]
 
     # Configure this pipeline step according to the used sensor
-    step = Gather()
+    var = "radiance"
+    step = Gather(var=var)
+    result = step.transform(values)
+
+    # Check that dimensions are those we expect
+    sensor_dims = {"spp_index": 1}
+    spectral_dims = {"w": 1}
+    film_dims = {
+        "y_index": 32,
+        "x_index": 32,
+    }  # Note: the 'channel' film dimension is dropped in mono mode
+    all_dims = {**sensor_dims, **spectral_dims, **film_dims}
+    assert dict(result.dims) == all_dims
+
+    # Check that data vars are those we expected
+    assert set(result.keys()) == {var, "spp"}
+    # Sensor data has all dimensions
+    assert set(result[var].dims) == set(all_dims.keys())
+    # SPP data has only sensor and spectral dimensions
+    assert set(result["spp"].dims) == set(
+        list(sensor_dims.keys()) + list(spectral_dims.keys())
+    )
+
+    # Check radiance and sample count values
+    assert np.allclose(2.0 / np.pi, result[var].values)
+    assert np.all([250] == result.spp.values)
+
+
+def test_pipeline_step_assemble_ckd(results_ckd):
+    """
+    Most basic test case for :class:`.Gather`:
+
+    * ckd mode;
+    * single-sensor measure;
+    * no sample count split.
+    """
+    values = results_ckd[0]
+
+    # Configure this pipeline step according to the used sensor
+    var = "radiance"
+    step = Gather(var=var)
+    result = step.transform(values)
+
+    # Check that dimensions are those we expect
+    sensor_dims = {"spp_index": 1}
+    spectral_dims = {"bin": 1, "index": 16}
+    film_dims = {
+        "y_index": 32,
+        "x_index": 32,
+    }  # Note: the 'channel' film dimension is dropped in mono mode
+    all_dims = {**sensor_dims, **spectral_dims, **film_dims}
+    assert dict(result.dims) == all_dims
+
+    # Check that data vars are those we expected
+    assert set(result.keys()) == {var, "spp"}
+    # Sensor data has all dimensions
+    assert set(result[var].dims) == set(all_dims.keys())
+    # SPP data has only sensor and spectral dimensions
+    assert set(result["spp"].dims) == set(
+        list(sensor_dims.keys()) + list(spectral_dims.keys())
+    )
+
+    # Check radiance and sample count values
+    assert np.allclose(2.0 / np.pi, result[var].values)
+    assert np.all([250] == result.spp.values)
+
+
+def test_pipeline_step_assemble_mono_spp(results_mono_spp):
+    """
+    Test case for :class:`.Gather` with
+
+    * mono mode;
+    * single-sensor measure;
+    * sample count split.
+    """
+    values = results_mono_spp[0]
+
+    # Configure this pipeline step according to the used sensor
+    var = "radiance"
+    step = Gather(var=var, sensor_dims=("spp",))
     result = step.transform(values)
 
     # Check that all dimensions are here as expected
-    assert result.dims == {
-        "w": 1,
-        "spp_index": 1,
+    sensor_dims = {"spp_index": 3}
+    spectral_dims = {"w": 1}
+    film_dims = {
         "y_index": 32,
         "x_index": 32,
-        "channel": 1,
-    }
-    assert set(result.keys()) == {"img", "spp"}
+    }  # Note: the 'channel' film dimension is dropped in mono mode
+    all_dims = {**sensor_dims, **spectral_dims, **film_dims}
+
+    # Check that data vars are those we expected
+    assert set(result.keys()) == {var, "spp"}
+    # Sensor data has all dimensions
+    assert set(result[var].dims) == set(all_dims.keys())
+    # SPP data has only sensor and spectral dimensions
+    assert set(result["spp"].dims) == set(
+        list(sensor_dims.keys()) + list(spectral_dims.keys())
+    )
 
     # Check radiance and sample count values
-    assert np.allclose(2.0 / np.pi, result.img.values)
-    assert np.all([250] == result.spp.values)
+    assert np.allclose(2.0 / np.pi, result[var].values)
+    assert np.all([100, 100, 50] == result.spp.values)
+
+
+def test_pipeline_step_assemble_ckd_spp(results_ckd_spp):
+    """
+    Test case for :class:`.Gather` with
+
+    * mono mode;
+    * single-sensor measure;
+    * sample count split.
+    """
+    values = results_ckd_spp[0]
+
+    # Configure this pipeline step according to the used sensor
+    var = "radiance"
+    step = Gather(var=var, sensor_dims=("spp",))
+    result = step.transform(values)
+
+    # Check that all dimensions are here as expected
+    sensor_dims = {"spp_index": 3}
+    spectral_dims = {"bin": 1, "index": 16}
+    film_dims = {
+        "y_index": 32,
+        "x_index": 32,
+    }  # Note: the 'channel' film dimension is dropped in mono mode
+    all_dims = {**sensor_dims, **spectral_dims, **film_dims}
+
+    # Check that data vars are those we expected
+    assert set(result.keys()) == {var, "spp"}
+    # Sensor data has all dimensions
+    assert set(result[var].dims) == set(all_dims.keys())
+    # SPP data has only sensor and spectral dimensions
+    assert set(result["spp"].dims) == set(
+        list(sensor_dims.keys()) + list(spectral_dims.keys())
+    )
+
+    # Check radiance and sample count values
+    assert np.allclose(2.0 / np.pi, result[var].values)
+    assert np.all([100, 100, 50] == result.spp.values)
+
+
+# Multi-sensor tests are removed until a multi-sensor measure is again available
+
+# def test_pipeline_step_gather_mono_ms_spp(results_mono_ms_spp):
+#     values = results_mono_ms_spp[0]
+#
+#     # Configure this pipeline step according to the used sensor
+#     step = Gather(sensor_dims=("ms", "spp"))
+#     result = step.transform(values)
+#
+#     # Check that all dimensions are here as expected
+#     assert result.dims == {
+#         "w": 1,
+#         "ms_index": 2,
+#         "spp_index": 3,
+#         "y_index": 1,
+#         "x_index": 1,
+#         # "channel": 1,  # channel dimension should be dropped in modes using mono variants
+#     }
+#     assert set(result.keys()) == {"img", "spp"}
+#
+#     # Check that variables have appropriate dimensions
+#     assert result.img.dims == {
+#         "w": 1,
+#         "ms_index": 2,
+#         "spp_index": 3,
+#         "y_index": 1,
+#         "x_index": 1,
+#     }
+#     assert result.spp.dims == {"spp_index", "w"}
+#
+#     # Check radiance and sample count values
+#     assert np.allclose(2.0 / np.pi, result.img.values)
+#     assert np.all([100, 100, 50] == result.spp.values)
+#
+#
+# def test_pipeline_step_assemble_ckd_ms_spp(results_ckd_ms_spp):
+#     values = results_ckd_ms_spp[0]
+#
+#     # Configure this pipeline step according to the used sensor
+#     step = Gather(sensor_dims=("ms", "spp"))
+#     result = step.transform(values)
+#
+#     # Check that all dimensions are here as expected
+#     assert result.dims == {
+#         "bin": 1,
+#         "index": 16,
+#         "ms_index": 2,
+#         "spp_index": 3,
+#         "y_index": 1,
+#         "x_index": 1,
+#         "channel": 1,
+#     }
+#     assert set(result.keys()) == {"img", "spp"}
+#
+#     # Check radiance and sample count values
+#     assert np.allclose(2.0 / np.pi, result.img.values)
+#     assert np.all([100, 100, 50] == result.spp.values)