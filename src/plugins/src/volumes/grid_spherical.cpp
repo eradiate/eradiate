5a0df7af883f4ecd64a77959e0de82305b9372a1
Sebastian Schunke <sebastian.schunke@gmx.net>
2022-01-07 17:21:29 +0100

    Updated the interface and simplified handling of transformation matrices

--- src/plugins/src/volumes/grid_spherical.cpp
+++ src/plugins/src/volumes/grid_spherical.cpp
@@ -16,9 +16,9 @@ enum class FilterType { Nearest, Trilinear };
 enum class WrapMode { Repeat, Mirror, Clamp };
 
 /**!
-.. _volume-gridvolume_spherical:
+.. _volume-sphericalgridvolume:
 
-Grid-based volume data source in spherical coordinates(:monosp:`gridvolume_spherical`)
+Grid-based volume data source in spherical coordinates(:monosp:`sphericalgridvolume`)
 ----------------------------------------------------------
 
 .. pluginparameters::
@@ -42,35 +42,45 @@ cover the [0,1] range.
 */
 
 template <typename Float, typename Spectrum>
-class GridVolumeSpherical final : public Volume<Float, Spectrum> {
+class SphericalGridVolume final : public Volume<Float, Spectrum> {
 public:
-    MTS_IMPORT_BASE(Volume, m_to_local, update_bbox)
+    MTS_IMPORT_BASE(Volume, m_to_local, m_bbox)
     MTS_IMPORT_TYPES(Volume, VolumeGrid)
 
-    GridVolumeSpherical(const Properties &props) : Base(props) {
+    SphericalGridVolume(const Properties &props) : Base(props) {
         m_gridvol = props.volume<Volume>("gridvolume", 1.f);
-        m_inner_bsphere_radius_relative = props.get<ScalarFloat>("inner_bsphere_radius", 0.f);
+
+        m_rmin = props.get<ScalarFloat>("rmin", 0.f);
+        m_rmax = props.get<ScalarFloat>("rmax", 1.f);
+        m_rmin_rel = m_rmin / m_rmax;
+
+        m_fillmin = props.get<ScalarFloat>("fillmin", 0.f);
+        m_fillmax = props.get<ScalarFloat>("fillmax", 0.f);
+
+        m_to_local = props.get<ScalarTransform4f>("to_world", ScalarTransform4f()).inverse();
+        update_bbox_sphere();
     }
 
     UnpolarizedSpectrum eval(const Interaction3f &it, Mask active) const override {
-        auto p = m_to_local * it.p;
-
-        // transform from the [0:1, 0:1, 0:1] cube to a  [-1:1, -1:1, -1:1] cube
-        ScalarTransform4f to_center = ScalarTransform4f::translate(ScalarPoint3f(-1, -1, -1)) * ScalarTransform4f::scale(2.f);
-        p = to_center * p;
+        Point3f p = m_to_local * it.p;
+        Float p_magnitude = ek::norm(p);
 
         Point3f p_spherical = Point3f(
-            (ek::norm(p) - m_inner_bsphere_radius_relative) / (1.f - m_inner_bsphere_radius_relative),
-            ek::acos(p.z() / ek::norm(p)) / ek::Pi<ScalarFloat>,
-            ek::atan2(p.y(), p.x()) / ek::Pi<ScalarFloat> + 1.f
+            (p_magnitude - m_rmin) / (m_rmax - m_rmin),
+            ek::acos(p.z() / p_magnitude) * ek::InvPi<ScalarFloat>,
+            ek::atan2(p.y(), p.x()) * ek::InvPi<ScalarFloat> + 1.f
         );
 
         Interaction3f it_spherical = it;
         it_spherical.p = p_spherical;
 
-        Mask within_spherical_shell = (m_inner_bsphere_radius_relative <= ek::norm(p) && ek::norm(p) <= 1.0);
-
-        return ek::select(within_spherical_shell, m_gridvol->eval(it_spherical, active), 0.f);
+        return ek::select(p_magnitude < m_rmin,
+            m_fillmin,
+            ek::select(p_magnitude > m_rmax,
+                m_fillmax,
+                m_gridvol->eval(it_spherical, active)
+            )
+        );
     }
 
     ScalarFloat max() const override { return m_gridvol->max(); }
@@ -80,11 +90,18 @@ public:
     MTS_DECLARE_CLASS()
 
 protected:
-    ScalarFloat m_inner_bsphere_radius_relative;
+    ScalarFloat m_center, m_rmin, m_rmax, m_rmin_rel, m_fillmin, m_fillmax;
     ref<Volume> m_gridvol;
+
+    void update_bbox_sphere() {
+        ScalarTransform4f to_world = m_to_local.inverse();
+        ScalarPoint3f a = to_world * ScalarPoint3f(-1.f, -1.f, -1.f);
+        ScalarPoint3f b = to_world * ScalarPoint3f(1.f, 1.f, 1.f);
+        m_bbox = ScalarBoundingBox3f(a, b);
+    }
 };
 
-MTS_IMPLEMENT_CLASS_VARIANT(GridVolumeSpherical, Volume)
-MTS_EXPORT_PLUGIN(GridVolumeSpherical, "GridVolumeSpherical texture")
+MTS_IMPLEMENT_CLASS_VARIANT(SphericalGridVolume, Volume)
+MTS_EXPORT_PLUGIN(SphericalGridVolume, "SphericalGridVolume texture")
 
 NAMESPACE_END(mitsuba)
