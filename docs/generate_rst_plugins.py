ec5ae191feea7be07de2af4a06d23787ac1da887
Vincent Leroy <vincent.leroy@rayference.eu>
2022-01-11 11:26:39 +0100

    Docs: Improve plugin docs generation

--- docs/generate_rst_plugins.py
+++ docs/generate_rst_plugins.py
@@ -1,18 +1,29 @@
 # This script walks through plugin files and extracts documentation that should
 # go into the reference manual.
-
+import glob
 import os
 import re
 from pathlib import Path
 
-# Section titles
+# Section titles (order matters)
 SECTIONS = {
+    "shapes": "Shapes",
     "bsdfs": "BSDFs",
+    "media": "Media",
+    "phase": "Phase functions",
+    "emitters": "Emitters",
     "sensors": "Sensors",
+    "textures": "Textures",
+    "spectra": "Spectra",
+    "volumes": "Volume data sources",
+    "integrators": "Integrators",
+    "samplers": "Samplers",
+    "films": "Films",
+    "rfilters": "Reconstruction filters",
 }
 
 # Plugin list
-PLUGINS = {
+PLUGIN_ORDERS = {
     "bsdfs": [
         "bilambertian",
         "rpv",
@@ -26,14 +37,17 @@ PLUGINS = {
 }
 
 
-def extract(in_filename, out_filename):
+def underline(s: str, c: str = "-"):
+    return c * len(s)
+
+
+def extract(in_filename: Path, out_filename: Path):
     with open(in_filename) as in_file, open(out_filename, "w") as out_file:
         inheader = False
 
         for line in in_file.readlines():
             match = re.match(r"^/\*\*! ?(.*)$", line)
             if match is not None:
-                print(f"Processing {in_filename}")
                 line = match.group(1).replace("%", "\%")
                 out_file.write(line + "\n")
                 inheader = True
@@ -56,7 +70,7 @@ def make_toc(section, title, out_filename):
 .. _sec-reference_plugins-{section}:
 
 {title}
--------
+{underline(title)}
 
 .. toctree::
    :maxdepth: 1
@@ -72,17 +86,42 @@ def generate():
     plugin_dir = root_dir / "src/plugins/src"
     out_dir = root_dir / "docs/rst/reference_plugins/generated"
 
-    for section, plugin_list in PLUGINS.items():
-        # Create TOC pages
+    for section, section_title in SECTIONS.items():
+        try:
+            plugin_order = PLUGIN_ORDERS[section]
+        except KeyError:
+            plugin_order = []
+
+        # Get plugin filenames
+        section_dir = plugin_dir / section
+        plugin_list = [
+            Path(filename).stem for filename in glob.glob(f"{section_dir}/*.cpp")
+        ]
+
+        if not plugin_list:
+            print(f"No plugin in section '{section}'")
+            continue
+
+        # Create TOC page
         section_rst = out_dir / f"toctrees/{section}.rst"
         os.makedirs(section_rst.parent, exist_ok=True)
-        make_toc(section, SECTIONS[section], section_rst)
+        make_toc(section, section_title, section_rst)
+
+        # Sort plugin list according to the ordering dict
+        def index(x):
+            try:
+                return plugin_order.index(x)
+            except ValueError:
+                return 1000
+
+        plugin_list.sort(key=index)
 
         # Extract plugin docs
         for plugin_name in plugin_list:
             plugin_cpp = plugin_dir / section / f"{plugin_name}.cpp"
             plugin_rst = out_dir / "plugins" / section / f"{plugin_name}.rst"
             os.makedirs(plugin_rst.parent, exist_ok=True)
+            print(f"Processing {Path(plugin_cpp).relative_to(root_dir)}")
             extract(plugin_cpp, plugin_rst)
 
 
