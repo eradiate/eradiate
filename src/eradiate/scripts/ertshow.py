d5d4b7d1a87399206ac92cb7b55648347e757311
Vincent Leroy <vincent.leroy@rayference.eu>
2022-01-12 16:04:17 +0100

    Add ertshow CLI script

---
+++ src/eradiate/scripts/ertshow.py
@@ -0,0 +1,24 @@
+import click
+from rich.console import Console
+
+import eradiate
+
+console = Console()
+
+
+@click.command()
+def cli():
+    """
+    Eradiate - Display information useful for debugging.
+    """
+    console.rule("Configuration")
+    for var in [x.name for x in eradiate.config.__attrs_attrs__]:
+        console.print(f"• ERADIATE_{var.upper()}: {getattr(eradiate.config, var)}")
+
+    console.rule("Path resolver")
+    for path in eradiate.path_resolver:
+        console.print(f"• {path}")
+
+
+if __name__ == "__main__":
+    cli()
