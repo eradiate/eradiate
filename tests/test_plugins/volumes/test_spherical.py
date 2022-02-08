bf5e65a4d7a86aba87cc94a8254e461df6e59160
Vincent Leroy <vincent.leroy@rayference.eu>
2022-02-08 11:15:48 +0100

    sphericalgridvolume: Rename to sphericalcoordsvolume, cleanup, more docs
    This commit polishes the `sphericalgridvolume` plugin. Changes are as
    follows:
    
    * The plugin was renamed to `sphericalcoordsvolume` to account for its
      ability to map texture coordinates for any volume data source, not
      just `gridvolume`.
    * The code was cleaned up and polished (unused variables and headers
      removed, more appropriate field naming, missing repr function).
    * Tests were updated with better handling of dimensional data and
      usage of default Mitsuba variants exclusively.
    * Plugin documentation was extended with further explanations and a
      mention of default values.

--- tests/test_plugins/volumes/test_spherical.py
+++ tests/test_plugins/volumes/test_spherical.py
@@ -1,10 +1,12 @@
+from pathlib import Path
+
 import numpy as np
 import pytest
 
 from eradiate.kernel.gridvolume import write_binary_grid3d
 
 
-def gridvol_constant(basepath, data=[[[1.0]]]):
+def gridvol_constant(basepath: Path, data: np.typing.ArrayLike = [[[1.0]]]):
     filename = basepath / "gridvol_const.vol"
     data = np.array(data)
     write_binary_grid3d(filename, data)
@@ -15,23 +17,23 @@ def test_construct(variant_scalar_rgb, tmp_path):
     from mitsuba.core import load_dict
 
     # Construct without parameters
-    volume = load_dict({"type": "sphericalgridvolume"})
+    volume = load_dict({"type": "sphericalcoordsvolume"})
     assert volume is not None
 
     # Construct with rmin > rmax:
     with pytest.raises(Exception):
-        load_dict({"type": "sphericalgridvolume", "rmin": 0.8, "rmax": 0.2})
+        load_dict({"type": "sphericalcoordsvolume", "rmin": 0.8, "rmax": 0.2})
 
     # Construct with all parameters set to typical values
     gridvol_filename = gridvol_constant(tmp_path)
     volume = load_dict(
         {
-            "type": "sphericalgridvolume",
+            "type": "sphericalcoordsvolume",
             "rmin": 0.0,
             "rmax": 1.0,
             "fillmin": 0.0,
             "fillmax": 0.0,
-            "gridvolume": {"type": "gridvolume", "filename": str(gridvol_filename)},
+            "volume": {"type": "gridvolume", "filename": str(gridvol_filename)},
         }
     )
     assert volume is not None
@@ -52,12 +54,12 @@ def test_eval_basic(variant_scalar_rgb, tmp_path, point, result):
     gridvol_filename = gridvol_constant(tmp_path, data=[[[2.0]]])
     volume = load_dict(
         {
-            "type": "sphericalgridvolume",
+            "type": "sphericalcoordsvolume",
             "rmin": 0.2,
             "rmax": 0.8,
             "fillmin": 3.0,
             "fillmax": 1.0,
-            "gridvolume": {"type": "gridvolume", "filename": str(gridvol_filename)},
+            "volume": {"type": "gridvolume", "filename": str(gridvol_filename)},
         }
     )
 
@@ -67,25 +69,22 @@ def test_eval_basic(variant_scalar_rgb, tmp_path, point, result):
     assert volume.eval(it) == result
 
 
-def test_eval_advanced(variant_scalar_mono, tmp_path):
+def test_eval_advanced(variant_scalar_rgb, tmp_path):
     from mitsuba.core import load_dict
     from mitsuba.render import Interaction3f
 
-    data = np.arange(1, 7, 1)
-    sigma_t = np.zeros((6, 2, 2))
-    sigma_t[:, 0, 0] = data
-    sigma_t[:, 0, 1] = data
-    sigma_t[:, 1, 0] = data
-    sigma_t[:, 1, 1] = data
-    gridvol_filename = gridvol_constant(tmp_path, data=sigma_t)
+    data = np.broadcast_to(
+        np.arange(1, 7, 1, dtype=float).reshape((-1, 1, 1, 1)), (6, 2, 2, 3)
+    )
+    gridvol_filename = gridvol_constant(tmp_path, data=data)
     volume = load_dict(
         {
-            "type": "sphericalgridvolume",
+            "type": "sphericalcoordsvolume",
             "rmin": 0.0,
             "rmax": 1.0,
             "fillmin": 3.0,
             "fillmax": 1.0,
-            "gridvolume": {
+            "volume": {
                 "type": "gridvolume",
                 "filename": str(gridvol_filename),
                 "filter_type": "nearest",
@@ -96,12 +95,17 @@ def test_eval_advanced(variant_scalar_mono, tmp_path):
     # have one point in each sector of the spherical volume
     # since the atan2 method maps [-pi, pi] to [0, 1], we choose this
     # order of inputs
-    angles = [-175, -115, -55, 5, 65, 125]
-    results = []
-    for angle in angles:
-        p = [0.5 * np.cos(np.deg2rad(angle)), 0.5 * np.sin(np.deg2rad(angle)), 0.0]
+    phis = np.array([-175, -115, -55, 5, 65, 125])  # degree
+    results = np.empty((len(phis), 3))
+
+    for i, phi in enumerate(phis):
+        p = [0.5 * np.cos(np.deg2rad(phi)), 0.5 * np.sin(np.deg2rad(phi)), 0.0]
         it = Interaction3f()
         it.p = p
-        results.append(volume.eval(it))
+        results[i, :] = volume.eval(it)
 
-    assert np.all(np.squeeze(results) == [1.0, 2.0, 3.0, 4.0, 5.0, 6.0])
+    expected = np.broadcast_to(
+        np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0]).reshape((-1, 1)),
+        (6, 3),
+    )
+    assert np.all(results == expected)
