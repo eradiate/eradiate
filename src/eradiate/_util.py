8cbd27fc8fb563f99a4805e0e504f83ccf3c8d4d
Vincent Leroy <vincent.leroy@rayference.eu>
2022-02-09 15:57:31 +0100

    util: Add fullname()

--- src/eradiate/_util.py
+++ src/eradiate/_util.py
@@ -1,3 +1,5 @@
+import functools
+import inspect
 import re
 import typing as t
 import warnings
@@ -237,3 +239,47 @@ class LoggingContext(object):
         if self.handler and self.close:
             self.handler.close()
         # implicit return of None => don't swallow exceptions
+
+
+def get_class_that_defined_method(meth: t.Any) -> t.Type:
+    """
+    Get the class which defined a method, if relevant. Otherwise, return
+    ``None``.
+    """
+    # See https://stackoverflow.com/questions/3589311/get-defining-class-of-unbound-method-object-in-python-3/25959545#25959545
+    if isinstance(meth, functools.partial):
+        return get_class_that_defined_method(meth.func)
+
+    if inspect.ismethod(meth) or (
+        inspect.isbuiltin(meth)
+        and getattr(meth, "__self__", None) is not None
+        and getattr(meth.__self__, "__class__", None)
+    ):
+        for cls in inspect.getmro(meth.__self__.__class__):
+            if meth.__name__ in cls.__dict__:
+                return cls
+        meth = getattr(meth, "__func__", meth)  # fallback to __qualname__ parsing
+
+    if inspect.isfunction(meth):
+        cls = getattr(
+            inspect.getmodule(meth),
+            meth.__qualname__.split(".<locals>", 1)[0].rsplit(".", 1)[0],
+            None,
+        )
+        if isinstance(cls, type):
+            return cls
+
+    return getattr(meth, "__objclass__", None)  # handle special descriptor objects
+
+
+def fullname(obj: t.Any) -> str:
+    """
+    Get the fully qualified name of `obj`. Aliases will be dereferenced.
+    """
+    cls = get_class_that_defined_method(obj)
+
+    if cls is None:
+        return f"{obj.__module__}.{obj.__qualname__}"
+
+    # else: # (it's a method)
+    return f"{cls.__module__}.{obj.__qualname__}"
