186ca62ff1c022fba1b0bb2330cd695e4d1abd34
Vincent Leroy <vincent.leroy@rayference.eu>
2022-04-13 16:19:58 +0200

    HeterogeneousAtmosphere: Add collision coefficient mixing test

--- tests/02_eradiate/01_unit/scenes/atmosphere/test_heterogeneous.py
+++ tests/02_eradiate/01_unit/scenes/atmosphere/test_heterogeneous.py
@@ -1,3 +1,5 @@
+import mitsuba as mi
+import numpy as np
 import pytest
 
 from eradiate import path_resolver
@@ -119,14 +121,69 @@ def test_heterogeneous_multi_ckd(mode_ckd, geometry, bin_set):
 
     ctx = KernelDictContext(spectral_ctx={"bin_set": bin_set})
 
-    # Kernel dict production succeeds
-    assert atmosphere.kernel_phase(ctx)
-    assert atmosphere.kernel_media(ctx)
-    assert atmosphere.kernel_shapes(ctx)
+    # Kernel dict production succeeds and produced data can be loaded
+    assert isinstance(atmosphere.kernel_phase(ctx).load(), mi.PhaseFunction)
+    assert isinstance(atmosphere.kernel_media(ctx).load(), mi.Medium)
+    assert isinstance(atmosphere.kernel_shapes(ctx).load(), mi.Shape)
 
-    # Produced kernel dict can be loaded
     kernel_dict = atmosphere.kernel_dict(ctx)
-    assert kernel_dict.load()
+    assert isinstance(kernel_dict.load(), mi.Scene)
+
+
+@pytest.mark.parametrize("field", ["sigma_a", "sigma_t"])
+def test_heterogeneous_mix_collision_coefficients(modes_all_double, field):
+    """
+    Check for component mixing correctness. We expect that the absorption and
+    extinction coefficients properly add up.
+    """
+    with ucc.override(length="km"):
+        component_1 = ParticleLayer(bottom=0.0, top=1.25)
+        component_2 = ParticleLayer(bottom=0.5, top=1.5)
+        component_3 = ParticleLayer(bottom=0.75, top=2.0)
+
+    mixed = HeterogeneousAtmosphere(
+        geometry="plane_parallel",
+        particle_layers=[component_1, component_2, component_3],
+    )
+    ctx = KernelDictContext()
+
+    radprofiles = {
+        component: atmosphere.eval_radprops(ctx.spectral_ctx)
+        for component, atmosphere in [
+            ("component_1", component_1),
+            ("component_2", component_2),
+            ("component_3", component_3),
+            ("mixed", mixed),
+        ]
+    }
+
+    collision_coefficient = {}
+    for z in [0.1, 0.6, 1.0, 1.4, 1.9] * ureg.km:
+        values = {}
+
+        for component, radprofile in radprofiles.items():
+            z_units = ureg.Unit(radprofile.coords["z_layer"].attrs["units"])
+
+            field_units = ureg(radprofile.data_vars["sigma_a"].attrs["units"])
+            values[component] = (
+                float(
+                    radprofile.data_vars[field].interp(
+                        z_layer=z.m_as(z_units),
+                        kwargs={"fill_value": 0.0},
+                        method="nearest",
+                    )
+                )
+                * field_units
+            )
+
+        collision_coefficient[z.m] = values
+
+    components = sorted(set(radprofiles.keys()) - {"mixed"})
+
+    for z in collision_coefficient.keys():
+        total = collision_coefficient[z]["mixed"]
+        expected = sum(collision_coefficient[z][component] for component in components)
+        assert np.allclose(expected, total), f"{z = }"
 
 
 def test_heterogeneous_scale(mode_mono, path_to_ussa76_approx_data):
@@ -140,7 +197,7 @@ def test_heterogeneous_scale(mode_mono, path_to_ussa76_approx_data):
         scale=2.0,
     ).kernel_dict(ctx)
     assert d["medium_atmosphere"]["scale"] == 2.0
-    assert d.load()
+    assert isinstance(d.load(), mi.Scene)
 
 
 def test_heterogeneous_blend_switches(mode_mono):
