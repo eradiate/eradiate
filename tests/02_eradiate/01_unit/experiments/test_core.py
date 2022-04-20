0bc9cf18593e3bb14d5bc2fcbcfa33a71881afb2
Vincent Leroy <vincent.leroy@rayference.eu>
2022-04-20 23:42:41 +0200

    Tests: Fix broken test for eradiate.run()

--- tests/02_eradiate/01_unit/experiments/test_core.py
+++ tests/02_eradiate/01_unit/experiments/test_core.py
@@ -67,6 +67,18 @@ def test_mitsuba_run_fail():
 
 
 def test_run_function(modes_all_double):
-    exp = OneDimExperiment(atmosphere=None)
+    mode = eradiate.mode()
+
+    measure = {"type": "mdistant"}
+    if mode.is_mono:
+        measure["spectral_cfg"] = {"wavelengths": [540.0, 550.0]}
+
+    elif mode.is_ckd:
+        measure["spectral_cfg"] = {"bin_set": "10nm", "bins": ["540", "550"]}
+
+    else:
+        assert False, f"Please add a test for mode '{mode.id}'"
+
+    exp = OneDimExperiment(atmosphere=None, measures=measure)
     result = eradiate.run(exp)
     assert isinstance(result, xr.Dataset)
