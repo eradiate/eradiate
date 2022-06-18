9e32cc7dca5365ca3bf07692564e8948b17a203c
Vincent Leroy <vincent.leroy@rayference.eu>
2022-06-18 11:35:22 +0200

    notebook: Extension also redirects logs

--- src/eradiate/notebook.py
+++ src/eradiate/notebook.py
@@ -41,6 +41,4 @@ def install(*extensions):
 
 # IPython extension. Must be imported by top-level module.
 def load_ipython_extension(ipython):
-    from eradiate.notebook import install
-
     install()
