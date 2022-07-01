f9c7ed42bf56512ba0906b0b69da7ad451e74e46
Vincent Leroy <vincent.leroy@rayference.eu>
2022-07-01 17:34:54 +0200

    DirectionalIllumination: Support azimuth convention specification

--- tests/02_eradiate/01_unit/scenes/illumination/test_directional.py
+++ tests/02_eradiate/01_unit/scenes/illumination/test_directional.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pinttr
 import pytest
 
@@ -31,3 +32,28 @@ def test_directional(mode_mono):
     assert d.kernel_dict(ctx).load() is not None
     with pytest.raises(pinttr.exceptions.UnitsError):  # Wrong units
         DirectionalIllumination(irradiance=ureg.Quantity(1.0, "W/m^2/sr/nm"))
+
+
+COS_PI_4 = np.sqrt(2) / 2
+
+
+@pytest.mark.parametrize(
+    "azimuth_convention, expected",
+    [
+        ("east_right", [0, -COS_PI_4, -COS_PI_4]),
+        ("east_left", [0, COS_PI_4, -COS_PI_4]),
+        ("north_right", [COS_PI_4, 0, -COS_PI_4]),
+        ("north_left", [-COS_PI_4, 0, -COS_PI_4]),
+        ("west_right", [0, COS_PI_4, -COS_PI_4]),
+        ("west_left", [0, -COS_PI_4, -COS_PI_4]),
+        ("south_right", [-COS_PI_4, 0, -COS_PI_4]),
+        ("south_left", [COS_PI_4, 0, -COS_PI_4]),
+    ],
+)
+def test_directional_azimuth_convention(mode_mono, azimuth_convention, expected):
+    illumination = DirectionalIllumination(
+        zenith=45 * ureg.deg,
+        azimuth=90 * ureg.deg,
+        azimuth_convention=azimuth_convention,
+    )
+    assert np.allclose(illumination.direction, expected), illumination.direction
