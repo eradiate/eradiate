aa4b720a3f07778239b1fac5436462f8a7acad3e
Vincent Leroy <vincent.leroy@rayference.eu>
2022-07-14 23:19:50 +0200

    data: Add offline data store initialisation

--- src/eradiate/data/_store.py
+++ src/eradiate/data/_store.py
@@ -1,5 +1,7 @@
+import typing as t
 from collections import OrderedDict
 
+from ._blind_directory import BlindDirectoryDataStore
 from ._blind_online import BlindOnlineDataStore
 from ._multi import MultiDataStore
 from ._safe_directory import SafeDirectoryDataStore
@@ -10,36 +12,70 @@ from .._config import config
 data_store: MultiDataStore = None
 
 
-def init_data_store():
+def init_data_store(offline: t.Optional[bool] = None) -> None:
     """
     Initialise the global data store.
+
+    Parameters
+    ----------
+    offline : bool, optional
+        If ``True``, replace all online data stores with blind directory data
+        stores. If unset, the global offline configuration is used.
     """
     global data_store
 
-    data_store = MultiDataStore(
-        stores=OrderedDict(
-            [
-                (
-                    "small_files",
-                    SafeDirectoryDataStore(path=config.source_dir / "resources/data/"),
-                ),
-                (
-                    "large_files_stable",
-                    SafeOnlineDataStore(
-                        base_url="http://eradiate.eu/data/store/stable/",
-                        path=config.download_dir / "stable",
+    if offline is None:
+        offline = config.offline
+
+    if not offline:
+        data_store = MultiDataStore(
+            stores=OrderedDict(
+                [
+                    (
+                        "small_files",
+                        SafeDirectoryDataStore(
+                            path=config.source_dir / "resources/data/"
+                        ),
                     ),
-                ),
-                (
-                    "large_files_unstable",
-                    BlindOnlineDataStore(
-                        base_url="http://eradiate.eu/data/store/unstable/",
-                        path=config.download_dir / "unstable",
+                    (
+                        "large_files_stable",
+                        SafeOnlineDataStore(
+                            base_url="http://eradiate.eu/data/store/stable/",
+                            path=config.download_dir / "stable",
+                        ),
                     ),
-                ),
-            ]
+                    (
+                        "large_files_unstable",
+                        BlindOnlineDataStore(
+                            base_url="http://eradiate.eu/data/store/unstable/",
+                            path=config.download_dir / "unstable",
+                        ),
+                    ),
+                ]
+            )
         )
-    )
+
+    else:
+        data_store = MultiDataStore(
+            stores=OrderedDict(
+                [
+                    (
+                        "small_files",
+                        SafeDirectoryDataStore(
+                            path=config.source_dir / "resources/data/"
+                        ),
+                    ),
+                    (
+                        "large_files_stable",
+                        BlindDirectoryDataStore(path=config.download_dir / "stable"),
+                    ),
+                    (
+                        "large_files_unstable",
+                        BlindDirectoryDataStore(path=config.download_dir / "unstable"),
+                    ),
+                ]
+            )
+        )
 
 
 # Initialise the data store upon module import
