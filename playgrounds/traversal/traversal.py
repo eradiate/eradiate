002881b04015552e6bec90153a3a971e3c575b07
Vincent Leroy <vincent.leroy@rayference.eu>
2022-11-21 00:03:40 +0100

    Traversal prototype

--- playgrounds/traversal/traversal.py
+++ playgrounds/traversal/traversal.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import enum
+import itertools
 import typing as t
 from collections import UserDict
 
@@ -195,35 +196,37 @@ class SceneElement:
     """
 
     @property
-    def template(self) -> dict:
+    def kernel_type(self) -> t.Optional[str]:
         """
-        Kernel dictionary template contents associated with this scene element.
+        Kernel type if this scene element can be modelled by a single kernel
+        scene graph node; ``None`` otherwise. The default implementation raises
+        a :class:`NotImplementedError`.
         """
         raise NotImplementedError
 
     @property
-    def params(self) -> t.Optional[t.Dict[str, Param]]:
+    def template(self) -> t.Mapping:
+        """
+        Kernel dictionary template contents associated with this scene element.
+        The default implementation raises a :class:`NotImplementedError`.
+        """
+        return {} if self.kernel_type is None else {"type": self.kernel_type}
+
+    @property
+    def params(self) -> t.Optional[t.Mapping[str, Param]]:
         """
         Map of updatable parameters associated with this scene element.
         """
         return None
 
     @property
-    def objects(self) -> t.Optional[t.Dict[str, SceneElement]]:
+    def objects(self) -> t.Optional[t.Mapping[str, SceneElement]]:
         """
         Map of child objects associated with this scene element.
         """
         return None
 
-    @property
-    def partials(self) -> t.Optional[t.List[SceneElement]]:
-        """
-        List of partial scene elements whose kernel dictionary templates and
-        update maps will be merged with this scene element's.
-        """
-        return None
-
-    def traverse(self, callback: SceneTraversal):
+    def traverse(self, callback: SceneTraversal) -> None:
         """
         Traverse this scene element and collect kernel dictionary template,
         parameter and object map contributions.
@@ -239,13 +242,12 @@ class SceneElement:
             callback.put_params(self.params)
 
         if self.objects is not None:
-            for name, obj in self.objects.items():
-                callback.put_object(name, obj)
-
-        # Merge partials if any
-        if self.partials is not None:
-            for obj in self.partials:
-                obj.traverse(callback)
+            if self.kernel_type is not None:
+                for name, obj in self.objects.items():
+                    callback.put_object(name, obj)
+            else:
+                for _, obj in self.objects.items():
+                    obj.traverse(callback)
 
 
 @attrs.define(eq=False)
@@ -253,8 +255,12 @@ class UniformSpectrum(SceneElement):
     value: float = attrs.field(default=1.0, converter=float)
 
     @property
-    def template(self):
-        return {"type": "uniform", "value": self.value}
+    def kernel_type(self) -> str:
+        return "uniform"
+
+    @property
+    def template(self) -> dict:
+        return {**self.template, "value": self.value}
 
 
 @attrs.define(eq=False)
@@ -262,15 +268,19 @@ class InterpolatedSpectrum(SceneElement):
     wavelengths: np.ndarray = attrs.field(converter=np.array)
     values: np.ndarray = attrs.field(converter=np.array)
 
+    @property
+    def kernel_type(self) -> t.Optional[str]:
+        return "uniform"
+
     def eval(self, /, w):
         return np.interp(w, self.wavelengths, self.values)
 
     @property
-    def template(self):
-        return {"type": "uniform", "value": EMPTY}
+    def template(self) -> t.Mapping:
+        return {**super().template, "value": EMPTY}
 
     @property
-    def params(self):
+    def params(self) -> t.Dict[str, Param]:
         return {"value": Param(self.eval, ParamFlags.SPECTRAL)}
 
 
@@ -281,11 +291,11 @@ class DiffuseBSDF(SceneElement):
     )
 
     @property
-    def template(self):
-        return {"type": "diffuse"}
+    def kernel_type(self) -> str:
+        return "diffuse"
 
     @property
-    def objects(self):
+    def objects(self) -> t.Dict[str, UniformSpectrum | InterpolatedSpectrum]:
         return {"reflectance": self.reflectance}
 
 
@@ -294,11 +304,11 @@ class RectangleShape(SceneElement):
     bsdf: DiffuseBSDF = attrs.field(factory=DiffuseBSDF)
 
     @property
-    def template(self):
-        return {"type": "rectangle"}
+    def kernel_type(self) -> str:
+        return "rectangle"
 
     @property
-    def objects(self):
+    def objects(self) -> t.Dict[str, DiffuseBSDF]:
         return {"bsdf": self.bsdf}
 
 
@@ -307,26 +317,37 @@ class MultiShape(SceneElement):
     shapes: t.List[RectangleShape] = attrs.field(factory=list)
 
     @property
-    def template(self):
-        return {}
+    def kernel_type(self) -> None:
+        return None
 
     @property
-    def objects(self):
+    def objects(self) -> t.Dict[str, RectangleShape]:
         return {f"shape_{i}": shape for i, shape in enumerate(self.shapes)}
 
 
 @attrs.define(eq=False)
 class PathIntegrator(SceneElement):
     @property
-    def template(self) -> dict:
-        return {"type": "path"}
+    def kernel_type(self) -> str:
+        return "path"
+
+
+@attrs.define
+class MonoSpectralConfig:
+    wavelengths: list = attrs.field(factory=lambda: [440.0, 550.0, 660.0])
 
 
 @attrs.define(eq=False)
 class PerspectiveCamera(SceneElement):
+    spectral_cfg = attrs.field(factory=MonoSpectralConfig)
+
     @property
-    def template(self) -> dict:
-        return {"type": "perspective"}
+    def kernel_type(self) -> str:
+        return "perspective"
+
+    @property
+    def spectral_indexes(self) -> list:
+        return self.spectral_cfg.wavelengths
 
 
 @attrs.define(eq=False)
@@ -335,8 +356,8 @@ class Scene(SceneElement):
     _partials: list = attrs.field(factory=list)
 
     @property
-    def template(self):
-        return {"type": "scene"}
+    def kernel_type(self) -> str:
+        return "scene"
 
     @property
     def objects(self):
@@ -392,7 +413,7 @@ class SceneTraversal:
         if node is None or node in self.hierarchy:
             return
 
-        cb = self.__class__(
+        cb = type(self)(
             node=node,
             parent=self.node,
             name=name if self.name is None else f"{self.name}.{name}",
@@ -421,24 +442,45 @@ class Experiment:
     kernel_scene: t.Optional["mitsuba.Scene"] = attrs.field(default=None, repr=False)
     results: dict = attrs.field(factory=dict)
 
+    @property
+    def measures(self) -> t.List[PerspectiveCamera]:
+        raise NotImplementedError
+
     def kernel_dict(self) -> KernelDict:
         raise NotImplementedError
 
-    def process(self):
+    def process(self, *measures, rebuild_kernel_scene: bool = False, spp=4):
         mi.set_variant("scalar_mono_double")
 
+        if not measures:
+            measures = self.measures
+
+        self.results.clear()
         kernel_dict = self.kernel_dict()
 
-        if self.kernel_scene is None:
+        if self.kernel_scene is None or rebuild_kernel_scene:
             print("Loading kernel scene")
             self.kernel_scene = mi.load_dict(kernel_dict.render(w=500.0))
         kernel_scene_params = mi.traverse(self.kernel_scene)
 
-        for w in [500, 600]:
+        print("Parametric loop")
+        spectral_indexes = sorted(
+            set(itertools.chain(*(m.spectral_indexes for m in measures)))
+        )
+
+        for w in spectral_indexes:
             print(f"{w = }")
+            print("Updating scene")
             update_map = kernel_dict.update_map.render(w=w)
             kernel_scene_params.update(update_map)
-            self.results["camera"] = mi.render(self.kernel_scene)
+
+            for sensor in self.kernel_scene.sensors():
+                loop_index = (w, sensor.id())
+
+                self.results[loop_index] = mi.render(
+                    self.kernel_scene, sensor=sensor, spp=spp
+                )
+                print()
 
         return self.results
 
@@ -447,7 +489,14 @@ class Experiment:
 class SomeExperiment(Experiment):
     shape: RectangleShape = attrs.field(kw_only=True)
     integrator: PathIntegrator = attrs.field(factory=PathIntegrator)
-    measure: PerspectiveCamera = attrs.field(factory=PerspectiveCamera)
+    measures: t.List[PerspectiveCamera] = attrs.field(
+        factory=PerspectiveCamera,
+        converter=lambda x: [x] if isinstance(x, PerspectiveCamera) else x,
+        validator=attrs.validators.deep_iterable(
+            member_validator=attrs.validators.instance_of(PerspectiveCamera),
+            iterable_validator=attrs.validators.instance_of(list),
+        ),
+    )
 
     def kernel_dict(self):
         return traverse(
@@ -455,7 +504,10 @@ class SomeExperiment(Experiment):
                 objects={
                     "shape": self.shape,
                     "integrator": self.integrator,
-                    "camera": self.measure,
+                    **{
+                        f"camera_{i}": measure
+                        for i, measure in enumerate(self.measures)
+                    },
                 }
             )
         )
