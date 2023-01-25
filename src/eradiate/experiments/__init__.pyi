563adf8db9d9601bef297a140a33256c1edc631d
Vincent Leroy <vincent.leroy@rayference.eu>
2023-01-25 11:23:37 +0100

    CanopyExperiment: Transition to new parametric loop

--- src/eradiate/experiments/__init__.pyi
+++ src/eradiate/experiments/__init__.pyi
@@ -1,4 +1,5 @@
 from ._atmosphere import AtmosphereExperiment as AtmosphereExperiment
+from ._canopy import CanopyExperiment as CanopyExperiment
 from ._core import EarthObservationExperiment as EarthObservationExperiment
 from ._core import Experiment as Experiment
 from ._core import mi_render as mi_render
