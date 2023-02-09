da4dc3b12d39464916de68d1834fd4645bd61be4
Vincent Leroy <vincent.leroy@rayference.eu>
2023-02-09 18:14:58 +0100

    mi_render(): Fix a bug where not all sensors were processed by default
    Contrary to what was stated in the docstring, the mi_render() function
    was not processing all sensors by default. This commit fixes this and
    adds a unit test to check for output correctness.

--- tests/02_eradiate/01_unit/kernel/test_render.py
+++ tests/02_eradiate/01_unit/kernel/test_render.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 import attrs
 import mitsuba as mi
 import numpy as np
@@ -178,3 +180,84 @@ def test_mi_render(mode_mono):
         actual.append(np.squeeze(result[w]["sensor"]))
 
     np.testing.assert_allclose(actual, expected)
+
+
+def test_mi_render_multisensor(mode_mono):
+    mi_scene = mi.load_dict(
+        {
+            "type": "scene",
+            "rectangle": {
+                "type": "rectangle",
+                "bsdf": {"type": "diffuse", "id": "my_bsdf"},
+            },
+            "sensor1": {
+                "type": "distant",
+                "film": {"type": "hdrfilm", "width": 1, "height": 1},
+                "direction": [0, 0, -1],
+                "target": [0, 0, 0],
+            },
+            "sensor2": {
+                "type": "distant",
+                "film": {"type": "hdrfilm", "width": 1, "height": 1},
+                "direction": [0, 0, -1],
+                "target": [0, 0, 0],
+            },
+            "illumination": {
+                "type": "directional",
+                "direction": [0, 0, -1],
+                "irradiance": 1.0,
+            },
+            "integrator": {"type": "path"},
+        }
+    )
+
+    umap_template = UpdateMapTemplate(
+        {
+            "my_bsdf.reflectance.value": UpdateParameter(
+                evaluator=lambda ctx: ctx.x,
+                flags=UpdateParameter.Flags.ALL,
+                lookup_strategy=TypeIdLookupStrategy(
+                    node_type=mi.BSDF,
+                    node_id="my_bsdf",
+                    parameter_relpath="reflectance.value",
+                ),
+            )
+        }
+    )
+
+    mi_wrapper = mi_traverse(mi_scene, umap_template)
+
+    @attrs.define
+    class ContextMockup:
+        x = attrs.field()
+        w = attrs.field()
+
+        @property
+        def index_formatted(self):
+            return str(self.x)
+
+        @property
+        def spectral_ctx(self):
+            return SpectralContext.new(wavelength=self.w)
+
+    reflectances = [0.0, 0.5, 1.0]
+    wavelengths = [400.0, 500.0, 600.0]
+
+    result = mi_render(
+        mi_wrapper,
+        ctxs=[ContextMockup(r, w) for (r, w) in zip(reflectances, wavelengths)],
+    )
+
+    # The result must be a nested dict with one level-one element per wavelength,
+    # and one level-two element per sensor
+    assert isinstance(result, dict)
+    assert list(result.keys()) == wavelengths
+
+    sensors_keys = set()
+    for spectral_key in result.keys():
+        sensors_keys.update(set(result[spectral_key].keys()))
+    assert sensors_keys == {"sensor1", "sensor2"}
+    assert all(
+        isinstance(result[spectral_key][sensor_key], mi.Bitmap)
+        for (spectral_key, sensor_key) in product(result.keys(), sensors_keys)
+    )
