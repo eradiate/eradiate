72790075f6aa39e9373712d6dca1f3038785d63b
Nicolas Misk <nicolas.misk@rayference.eu>
2023-06-15 08:57:28 +0000

    Implement a dependency layer graph outside of pyproject.toml; traverse the graph to create pip/conda locks

--- requirements/make_conda_env.py
+++ requirements/make_conda_env.py
@@ -1,27 +1,38 @@
 import os
 import sys
 from copy import deepcopy
+import networkx as nx
 
 import click
 from ruamel.yaml import YAML
 from ruamel.yaml.comments import CommentedSeq as CS
-from setuptools.config.pyprojecttoml import read_configuration
 
 
 @click.command()
 @click.option(
     "-s",
     "--sections",
-    default="main,recommended,tests,dev,docs",
+    default="dependencies,main,recommended,tests,dev,docs,optional",
     help="Dependency sections to include in the produced environment.yml file. "
-    "Default: 'main,recommended,tests,dev,docs'",
+    "Default: 'dependencies,main,recommended,tests,dev,docs,optional'",
 )
+@click.option("-o", "--output-dir", default="requirements/conda", help="Path to output file.")
 @click.option(
-    "-i", "--input", default="pyproject.toml", help="Path to pyproject.toml file."
+    "-l",
+    "--layered-config",
+    default="./requirements/layered.yml",
+    help="Path to layered requirement dependency configuration file. "
+    "Default: ./requirements/layered.yml",
 )
-@click.option("-o", "--output", default=None, help="Path to output file.")
 @click.option("-q", "--quiet", is_flag=True, help="Suppress terminal output.")
-def cli(sections, input, output, quiet):
+@click.option(
+    "-g",
+    "--write-graphs",
+    is_flag=True,
+    default=False,
+    help="Write dependency graphs to Graphviz dot files. Default: False"
+)
+def cli(sections, output_dir, layered_config, quiet, write_graphs):
     """
     Create a Conda environment file from a pyproject.toml.
     """
@@ -34,54 +45,91 @@ def cli(sections, input, output, quiet):
     # Load environment file template
     with open(os.path.join("requirements", "environment.in")) as f:
         env_yml = yaml.load(f.read())
+    with open(layered_config) as f:
+        layered_yml = yaml.load(f.read())
 
-    # Extract dependency section contents
-    if not quiet:
-        print(f"Reading dependencies from {input}")
-
-    setup_config = read_configuration(input)
-    sections = [x.strip() for x in sections.split(",")]
-    section_indices = dict()
+    sections = [
+        (x.strip(), {"packages": set(layered_yml[x.strip()].get("packages", []))})
+        for x in sections.split(",")
+    ]
     dep_list = deepcopy(env_yml["dependencies"]) if "dependencies" in env_yml else []
-    i = len(dep_list)
+
+    # Create dependency graph
+    G = nx.DiGraph()
+    G.add_nodes_from(sections)
+    for section, data in sections:
+        for include in layered_yml[section].get("includes", []):
+            G.add_edge(section, include)
+
+    if write_graphs:
+      path = os.path.join(output_dir, f"layer_graph.dot")
+      nx.drawing.nx_pydot.write_dot(G, path)
+
+    G.add_node("default", packages=set(dep_list))
+    G.add_edge("main", "default")
+
+    if not nx.algorithms.dag.is_directed_acyclic_graph(G):
+        raise ValueError("Dependency graph is not a DAG")
+
+    sections = list(nx.algorithms.dag.topological_sort(G))[::-1]
 
     for section in sections:
-        section_indices[section] = i
+        # Select sections to include
+        subG = G.subgraph(nx.algorithms.dag.descendants(G, section) | {section})
+        environment_packages = dict(sorted(subG.nodes.data()))
 
-        try:
-            packages = (
-                setup_config["project"]["dependencies"]
-                if section == "main"
-                else setup_config["project"]["optional-dependencies"][section]
-            )
-        except KeyError:
-            raise RuntimeError(f"Cannot fetch dependencies from {input}")
+        # Create dependency list
+        section_indices = dict()
+        added_packages = set()
+        ordered_packages = []
+        for included_section, data in environment_packages.items():
+            packages_to_add = sorted(data["packages"] - added_packages)
 
-        for package in packages:
-            if package not in dep_list:  # Do not duplicate
-                dep_list.append(package)
-                i += 1
+            # Temporary fix until an eradiate_mitsuba conda package is made available
+            mitsuba_pkg = set(p for p in packages_to_add if "eradiate-mitsuba" in p)
+            if mitsuba_pkg:
+                packages_to_add = [p for p in packages_to_add if p not in mitsuba_pkg]
 
-    # Format dependency list
-    lst = CS(dep_list)
+            section_indices[included_section] = len(ordered_packages)
+            if packages_to_add:
+                ordered_packages.extend(packages_to_add)
 
-    for section, i in section_indices.items():
-        lst.yaml_set_comment_before_after_key(i, section, indent_offset)
+            # Temporary fix until an eradiate_mitsuba conda package is made available
+            if mitsuba_pkg:
+                if "pip" not in added_packages:
+                    ordered_packages.append("pip")
+                ordered_packages.append({"pip": list(mitsuba_pkg)})
+            
+            added_packages |= data["packages"]
 
-    env_yml["dependencies"] = lst
+        if section == "default":
+            continue
 
-    # Output to terminal
-    if not quiet:
-        yaml.dump(env_yml, sys.stdout)
+        # Format dependency list
+        lst = CS(ordered_packages)
+        for included_section, i in section_indices.items():
+            lst.yaml_set_comment_before_after_key(i, included_section, indent_offset)
+        env_yml["dependencies"] = lst
 
-    # Output to file
-    if output is not None:
-        with open(output, "w") as outfile:
-            if not quiet:
-                print()
-            print(f"Saving to {output}")
-            yaml.dump(env_yml, outfile)
+        # Output to terminal
+        if not quiet:
+            yaml.dump(env_yml, sys.stdout)
 
+        # Output to file
+        if output_dir is not None:
+            path = os.path.join(output_dir, f"environment-{section}.yml")
+            with open(path, "w") as outfile:
+                if not quiet:
+                    print()
+                print(f"Saving to {path}")
+                yaml.dump(env_yml, outfile)
+
+        if write_graphs:
+            path = os.path.join(output_dir, f"environment-{section}.dot")
+            if "default" in subG:
+                subG = subG.copy()
+                subG.remove_node("default")
+            nx.drawing.nx_pydot.write_dot(subG, path)
 
 if __name__ == "__main__":
     cli()
--- requirements/make_pip_in_files.py
+++ requirements/make_pip_in_files.py
@@ -2,28 +2,21 @@ import os
 
 import click
 from ruamel.yaml import YAML
-from setuptools.config.pyprojecttoml import read_configuration
 
 
 @click.command()
 @click.option(
     "-s",
     "--sections",
-    default="main,recommended,tests,dev,docs",
+    default="dependencies,main,recommended,tests,dev,docs,optional",
     help="Dependency sections to include in the produced environment.yml file. "
-    "Default: 'main,recommended,tests,dev,docs'",
-)
-@click.option(
-    "-i",
-    "--input",
-    default="pyproject.toml",
-    help="Path to pyproject.toml file. Default: pyproject.toml",
+    "Default: 'dependencies,main,recommended,tests,dev,docs,optional'",
 )
 @click.option(
     "-o",
     "--output-dir",
-    default="./requirements",
-    help="Path to output directory. Default: ./requirements",
+    default="./requirements/pip",
+    help="Path to output directory. Default: ./requirements/pip",
 )
 @click.option(
     "-l",
@@ -33,12 +26,9 @@ from setuptools.config.pyprojecttoml import read_configuration
     "Default: ./requirements/layered.yml",
 )
 @click.option("-q", "--quiet", is_flag=True, help="Suppress terminal output.")
-def cli(sections, input, output_dir, layered_config, quiet):
+def cli(sections, output_dir, layered_config, quiet):
     # Load dependency list
-    if not quiet:
-        print(f"Reading dependencies from {input}")
 
-    setup_config = read_configuration(input)
     sections = [x.strip() for x in sections.split(",")]
 
     # Load layered dependency dependencies
@@ -50,14 +40,7 @@ def cli(sections, input, output_dir, layered_config, quiet):
         if not quiet:
             print(f"Processing section '{section}'")
 
-        try:
-            packages = (
-                setup_config["project"]["dependencies"]
-                if section == "main"
-                else setup_config["project"]["optional-dependencies"][section]
-            )
-        except KeyError:
-            raise RuntimeError(f"Cannot fetch dependencies from {input}")
+        packages = layered_yml[section].get("packages", [])
 
         if not quiet:
             print(f"Writing to {os.path.join(output_dir, f'{section}.in')}")
