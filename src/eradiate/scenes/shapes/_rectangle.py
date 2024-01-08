b93dc24d6d333dead9aebb3f0f618a7ea4995d18
Sebastian Schunke <sebastian.schunke@rayference.eu>
2024-01-08 17:51:50 +0100

    WIP, buffermesh and tests still open

--- src/eradiate/scenes/shapes/_rectangle.py
+++ src/eradiate/scenes/shapes/_rectangle.py
@@ -104,14 +104,18 @@ class RectangleShape(ShapeNode):
         # Inherit docstring
         length_units = uck.get("length")
         scale = self.edges.m_as(length_units) * 0.5
-        result = {
-            "type": "rectangle",
-            "to_world": mi.ScalarTransform4f.look_at(
+        if self.to_world is not None:
+            trafo = self.to_world
+        else:
+            trafo = mi.ScalarTransform4f.look_at(
                 origin=self.center.m_as(length_units),
                 target=self.center.m_as(length_units) + self.normal,
                 up=self.up,
-            )
-            @ mi.ScalarTransform4f.scale([scale[0], scale[1], 1.0]),
+            ) @ mi.ScalarTransform4f.scale([scale[0], scale[1], 1.0])
+
+        result = {
+            "type": "rectangle",
+            "to_world": trafo,
         }
 
         return result
