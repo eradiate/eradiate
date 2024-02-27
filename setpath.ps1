f0c3c6a2465f3e22d7f23e1ab3a603bce47fe3a3
Vincent Leroy <vincent.leroy@rayference.eu>
2024-02-27 12:42:07 +0100

    Tooling changes required to build on Windows
    This commit updates make targets and adds additional environment 
    variable setup scripts to accommodate a Windows development environment.

---
+++ setpath.ps1
@@ -0,0 +1,9 @@
+# ***************************************************************
+# * This script adds Eradiate to the current path on Windows.
+# * It assumes that Mitsuba is compiled within a subdirectory
+# * named 'build'.
+# ***************************************************************
+
+$env:ERADIATE_SOURCE_DIR = Get-Location
+$env:PATH = $env:ERADIATE_SOURCE_DIR + "\ext\mitsuba\Release\build" + ";" + $env:PATH
+$env:PYTHONPATH = $env:ERADIATE_SOURCE_DIR + "\ext\mitsuba\build\Release\python" + ";" + $env:PYTHONPATH
