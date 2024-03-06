1487a85eea1c3e0f8712c0ee4d124682e3c8b637
Lucio Franceschini <lucio.franceschini@rayference.eu>
2024-03-06 09:38:12 +0000

    Add Scene Loader and RAMI Scenarios Support
    - Implemented a new scene loader functionality for Eradiate that allows for the loading of `scenario.json` files to define complex scenes, including surface descriptions and canopy configurations.
    - Introduced support for preconfigured RAMI-V scenarios, leveraging the new scene loader functionality.

---
+++ src/eradiate/scenes/biosphere/_canopy_loader.py
@@ -0,0 +1,241 @@
+from __future__ import annotations
+
+import json
+import typing as t
+from pathlib import Path
+
+import numpy as np
+
+SCENARIO_FILE_NAME = "scenario.json"
+
+
+def _update_material(
+    elem: dict,
+    canopy_name: str,
+    spectral_data: dict[str, t.Any | dict[str, t.Any]] | None,
+) -> dict:
+    """
+    Update the material of an element with spectral data if available.
+
+    Parameters
+    ----------
+    elem : dict
+        The element to update.
+    canopy_name : str
+        The name of the canopy or ground.
+    spectral_data : dict[str, t.Any | dict[str, t.Any]] | None
+        The spectral data to apply to the element.
+
+    Returns
+    -------
+    dict
+        The updated element.
+    """
+    elem_copy = elem.copy()
+    if (
+        spectral_data is not None
+        and canopy_name in spectral_data
+        and (canopy_name == "ground" or elem["id"] in spectral_data[canopy_name])
+    ):
+        assert isinstance(spectral_data, dict)
+        try:
+            del elem_copy["reflectance"]
+        except KeyError:
+            pass
+        try:
+            del elem_copy["transmittance"]
+        except KeyError:
+            pass
+        if canopy_name == "ground":
+            return {**elem_copy, **spectral_data[canopy_name]}
+        else:
+            assert isinstance(spectral_data[canopy_name], dict)
+            return {**elem_copy, **spectral_data[canopy_name][elem["id"]]}
+    else:
+        return elem
+
+
+def _parse_rpv_surface(
+    surface: dict[str, t.Any], spectral_data: dict[str, t.Any]
+) -> dict[str, t.Any]:
+    """
+    Convert surface data to RPV representation.
+
+    Parameters
+    ----------
+    surface : dict
+        Surface data to convert.
+    spectral_data : dict
+        Spectral data to apply to the surface.
+
+    Returns
+    -------
+    dict
+        RPV representation of the surface data.
+    """
+    return {
+        **surface,
+        **_update_material(
+            {"rho_0": surface.get("rho_0", {})},
+            "ground",
+            spectral_data,
+        ),
+        **_update_material(
+            {"g": surface.get("g", {})},
+            "ground",
+            spectral_data,
+        ),
+        **_update_material(
+            {"k": surface.get("k", {})},
+            "ground",
+            spectral_data,
+        ),
+    }
+
+
+def _parse_lambertian_surface(
+    surface: dict[str, t.Any], spectral_data: dict[str, t.Any]
+) -> dict[str, t.Any]:
+    """
+    Convert surface data to reflectance representation.
+
+    Parameters
+    ----------
+    surface : dict
+        Surface data to convert.
+    spectral_data : dict
+        Spectral data to apply to the surface.
+
+    Returns
+    -------
+    dict
+        Reflectance representation of the surface data.
+    """
+    return {
+        **surface,
+        **_update_material(
+            {"reflectance": surface.get("reflectance", {})},
+            "ground",
+            spectral_data,
+        ),
+        **(
+            _update_material(
+                {"transmittance": surface.get("transmittance", {})},
+                "ground",
+                spectral_data,
+            )
+            if "transmittance" in surface
+            else {}
+        ),
+    }
+
+
+def apply_transformation(transf: np.ndarray, center: np.ndarray) -> np.ndarray:
+    """
+    Apply transformation matrix to origin and translate the instance positions to the center of the scenario.
+
+    Parameters
+    ----------
+    transf : np.ndarray
+        Array of transformation values.
+    center : np.ndarray
+        Array representing the center of the scenario.
+
+    Returns
+    -------
+    np.ndarray
+        Transformed positions with center adjustment.
+    """
+    origin = np.array([0.0, 0.0, 0.0, 1.0]).T
+
+    # Apply the transformation matrix to origin and adjust the center
+    return (transf @ origin)[:3].T - center
+
+
+def load_scenario(
+    scenario_folder: Path,
+    padding: int,
+    spectral_data: dict[str, t.Any | dict[str, t.Any]] | None = None,
+) -> dict:
+    """
+    Parse JSON file of scenario from a given path. Apply transformation to the data by converting units of wavelengths and
+    compute instance positions.
+
+    Parameters
+    ----------
+    scenario_folder : Path
+        Path of the folder containing scenario JSON file.
+    padding : int
+        Padding to apply to the scenario.
+    spectral_data : dict[str, t.Any | dict[str, t.Any]] | None
+        Spectral data to apply to the scenario, defaults to None (keep original).
+
+        An example of the spectral data is given below:
+
+        spectral_data = {
+            "ground": {
+                "reflectance": ground_reflectance,
+            },
+            "object_name": {
+                "subobject_name": {
+                    # Spectral data for the subobject, such as
+                    "reflectance": reflectance,
+                    "transmittance": transmittance,
+                },
+            }
+        }
+
+        Each spectral data specified replaces the original data completely, so it is necessary to specify all the data
+        for the object.
+
+    Returns
+    -------
+    t.Dict
+        Returns a dictionary parsed from JSON with transformations applied.
+    """
+
+    # Load "scenario.json" as dictionary object
+    scenario = json.loads((scenario_folder / SCENARIO_FILE_NAME).read_text())
+
+    # Update dictionary elements with transformations
+    surface = scenario["surface"]
+    size = scenario["canopy"]["size"]
+    center_2d = np.array([size[0], size[1], 0.0]) / 2
+    return {
+        **scenario,
+        "surface": (
+            _parse_rpv_surface(surface, spectral_data)
+            if surface["type"] == "rpv"
+            else _parse_lambertian_surface(surface, spectral_data)
+        ),
+        "canopy": {
+            **scenario["canopy"],
+            "instanced_canopy_elements": [
+                {
+                    **elem,
+                    "instance_positions": [
+                        apply_transformation(transf, center_2d)
+                        for transf in elem["instance_positions"]
+                    ],
+                    "canopy_element": {
+                        **elem["canopy_element"],
+                        "mesh_tree_elements": [
+                            _update_material(
+                                {
+                                    **tree,
+                                    "mesh_filename": (
+                                        scenario_folder / tree["mesh_filename"]
+                                    ),
+                                },
+                                canopy_name=elem["canopy_element"]["id"],
+                                spectral_data=spectral_data,
+                            )
+                            for tree in elem["canopy_element"]["mesh_tree_elements"]
+                        ],
+                    },
+                }
+                for elem in scenario["canopy"]["instanced_canopy_elements"]
+            ],
+            "padding": padding,
+        },
+    }
---
+++ src/eradiate/scenes/biosphere/_rami_scenarios.py
@@ -0,0 +1,183 @@
+from __future__ import annotations
+
+import itertools
+import typing
+import zipfile
+from enum import Enum
+from pathlib import Path
+
+from ..biosphere import load_scenario
+
+DEFAULT_SCENARIO_FOLDER_NAME = ".scenarios"
+
+
+class RAMIActualCanopies(Enum):
+    JARVSELJA_PINE_STAND = "HET07_JPS_SUM"  # Summer
+    OFENPASS_PINE_STAND = "HET08_OPS_WIN"  # Winter
+    JARVSELJA_BIRCH_STAND_SUMMER = "HET09_JBS_SUM"  # Summer
+    WELLINGTON_CITRUS_ORCHARD = "HET14_WCO_UND"
+    JARVSELJA_BIRCH_STAND_WINTER = "HET15_JBS_WIN"  # Winter
+    AGRICULTURAL_CROPS = "HET16_SRF_UND"  # Short Rotation Forest
+    SAVANNA_PRE_FIRE = "HET50_SAV_PRE"  # Semi-empirical
+    WYTHAM_WOOD = "HET51_WWO_TLS"  # Empirical
+
+
+class RAMIHeterogeneousAbstractCanopies(Enum):
+    ANISOTROPIC_BACKGROUND_OVERSTOREY_SPARSE_BRF_MODEL_A = "HET10_DIS_S1A"
+    ANISOTROPIC_BACKGROUND_OVERSTOREY_SPARSE_BRF_MODEL_B = "HET11_DIS_S1B"
+    ANISOTROPIC_BACKGROUND_OVERSTOREY_SPARSE_BRF_MODEL_C = "HET12_DIS_S1C"
+    ANISOTROPIC_BACKGROUND_OVERSTOREY_DENSE_BRF_MODEL_A = "HET20_DIS_D1A"
+    ANISOTROPIC_BACKGROUND_OVERSTOREY_DENSE_BRF_MODEL_B = "HET21_DIS_D1B"
+    ANISOTROPIC_BACKGROUND_OVERSTOREY_DENSE_BRF_MODEL_C = "HET22_DIS_D1C"
+    TWO_LAYER_CANOPY_OVERSTORIES_SPARSE_UNDERSTORIES_SPARSE = "HET16_DIS_S2S"
+    TWO_LAYER_CANOPY_OVERSTORIES_MEDIUM_UNDERSTORIES_SPARSE = "HET17_DIS_M2S"
+    TWO_LAYER_CANOPY_OVERSTORIES_DENSE_UNDERSTORIES_SPARSE = "HET18_DIS_D2S"
+    TWO_LAYER_CANOPY_OVERSTORIES_SPARSE_UNDERSTORIES_DENSE = "HET26_DIS_S2D"
+    TWO_LAYER_CANOPY_OVERSTORIES_MEDIUM_UNDERSTORIES_DENSE = "HET27_DIS_M2D"
+    TWO_LAYER_CANOPY_OVERSTORIES_DENSE_UNDERSTORIES_DENSE = "HET28_DIS_D2D"
+    CONSTANT_SLOPE_DISTRIBUTION_SPARSE_INCLINATION_15 = "HET23_DIS_S15"
+    CONSTANT_SLOPE_DISTRIBUTION_DENSE_INCLINATION_15 = "HET24_DIS_D15"
+    CONSTANT_SLOPE_DISTRIBUTION_SPARSE_INCLINATION_30 = "HET33_DIS_S30"
+    CONSTANT_SLOPE_DISTRIBUTION_DENSE_INCLINATION_30 = "HET34_DIS_D30"
+
+
+class RAMIHomogeneousAbstractCanopies(Enum):
+    ANISOTROPIC_BACKGROUND_PLANOPHILE_A = "HOM23_DIS_P1A"
+    ANISOTROPIC_BACKGROUND_PLANOPHILE_B = "HOM24_DIS_P1B"
+    ANISOTROPIC_BACKGROUND_PLANOPHILE_C = "HOM25_DIS_P1C"
+    # ANISOTROPIC_BACKGROUND_ERECTOPHILE_A = "HOM33_DIS_E1A"
+    ANISOTROPIC_BACKGROUND_ERECTOPHILE_B = "HOM34_DIS_E1B"
+    ANISOTROPIC_BACKGROUND_ERECTOPHILE_C = "HOM35_DIS_E1C"
+    TWO_LAYER_CANOPY_ERECTOPHILE_SPARSE_PLANOPHILE_DENSE = "HOM26_DIS_EPD"
+    TWO_LAYER_CANOPY_ERECTOPHILE_SPARSE_PLANOPHILE_MEDIUM = "HOM27_DIS_EPM"
+    TWO_LAYER_CANOPY_ERECTOPHILE_SPARSE_PLANOPHILE_SPARSE = "HOM28_DIS_EPS"
+    TWO_LAYER_CANOPY_PLANOPHILE_SPARSE_ERECTOPHILE_DENSE = "HOM36_DIS_PED"
+    TWO_LAYER_CANOPY_PLANOPHILE_SPARSE_ERECTOPHILE_MEDIUM = "HOM37_DIS_PEM"
+    TWO_LAYER_CANOPY_PLANOPHILE_SPARSE_ERECTOPHILE_SPARSE = "HOM38_DIS_PES"
+    ADJACENT_CANOPIES_SPARSE_ERECTOPHILE_DENSE_PLANOPHILE = "HOM29_DIS_EM0"
+    ADJACENT_CANOPIES_MEDIUM_ERECTOPHILE_SPARSE_PLANOPHILE = "HOM30_DIS_ED0"
+
+
+class RAMIScenarioVersion(Enum):
+    ORIGINAL = "original"
+    SIMPLIFIED = "simplified"
+
+
+def generate_name(
+    scenario_name: (
+        RAMIActualCanopies
+        | RAMIHeterogeneousAbstractCanopies
+        | RAMIHomogeneousAbstractCanopies
+    ),
+    version: RAMIScenarioVersion = RAMIScenarioVersion.ORIGINAL,
+) -> str:
+    """
+    Generate a name for a scenario based on its name and version.
+
+    Parameters
+    ----------
+    scenario_name : RAMIActualCanopies or RAMIHeterogeneousAbstractCanopies or RAMIHomogeneousAbstractCanopies
+        The name of the scenario.
+    version : ScenarioVersion
+        The version of the scenario.
+
+    Returns
+    -------
+    str
+        The name of the scenario.
+    """
+    return (
+        f"{scenario_name.value}-{version.value}"
+        if version == RAMIScenarioVersion.SIMPLIFIED
+        else scenario_name.value
+    )
+
+
+def _convert_to_enum(
+    scenario_name: (
+        str
+        | RAMIActualCanopies
+        | RAMIHeterogeneousAbstractCanopies
+        | RAMIHomogeneousAbstractCanopies
+    ),
+) -> (
+    RAMIActualCanopies
+    | RAMIHeterogeneousAbstractCanopies
+    | RAMIHomogeneousAbstractCanopies
+):
+    """
+    Convert a scenario name to an enum if it is a string.
+
+    Parameters
+    ----------
+    scenario_name : str or RAMIActualCanopies or RAMIHeterogeneousAbstractCanopies or RAMIHomogeneousAbstractCanopies
+        The name of the scenario.
+
+    Returns
+    -------
+    (RAMIActualCanopies | RAMIHeterogeneousAbstractCanopies | RAMIHomogeneousAbstractCanopies)
+        The name of the scenario as an enum.
+    """
+
+    if isinstance(scenario_name, str):
+        for member in itertools.chain.from_iterable(
+            [
+                RAMIActualCanopies,
+                RAMIHeterogeneousAbstractCanopies,
+                RAMIHomogeneousAbstractCanopies,
+            ]
+        ):
+            if scenario_name == member.value:
+                return member
+        else:
+            raise ValueError(f"Scenario {scenario_name} not found")
+    else:
+        return scenario_name
+
+
+def load_rami_scenario(
+    scenario_name: (
+        str
+        | RAMIActualCanopies
+        | RAMIHeterogeneousAbstractCanopies
+        | RAMIHomogeneousAbstractCanopies
+    ),
+    version: RAMIScenarioVersion = RAMIScenarioVersion.ORIGINAL,
+    padding: int = 0,
+    unpack_folder: typing.Optional[Path] = None,
+) -> dict:
+    """
+    Load a scenario based on its name and version.
+
+    Parameters
+    ----------
+    scenario_name : str or RAMIActualCanopies or RAMIHeterogeneousAbstractCanopies or RAMIHomogeneousAbstractCanopies
+        The name of the RAMI-V scenario. If a string is provided, it will automatically be converted to the appropriate enum.
+    version : ScenarioVersion
+        The version of the scenario.
+    padding : int, optional
+        The padding to apply to the scenario, defaults to 0.
+    unpack_folder : Path, optional
+        The folder to unpack the scenario to, defaults to None (current working directory).
+
+    Returns
+    -------
+    dict
+        The scenario.
+    """
+    from eradiate.data import data_store
+
+    name = f"scenarios/rami5/{generate_name(_convert_to_enum(scenario_name), version)}"
+
+    if unpack_folder is None:
+        unpack_folder = Path.cwd() / DEFAULT_SCENARIO_FOLDER_NAME
+
+    scenario_folder = unpack_folder / name
+    if not scenario_folder.exists():
+        local_path = data_store.fetch(f"{name}.zip")
+        # Unzip the scenario
+        with zipfile.ZipFile(local_path, "r") as zip_ref:
+            zip_ref.extractall(scenario_folder)
+
+    # Load the scenario
+    return load_scenario(scenario_folder, padding)
