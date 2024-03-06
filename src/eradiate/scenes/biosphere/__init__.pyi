06bb1394a55eb93b945bcdcf3ac0e5c4f47e0fc4
Lucio Franceschini <lucio.franceschini@rayference.eu>
2024-03-06 09:38:12 +0000

    Add Scene Loader and RAMI Scenarios Support
    - Implemented a new scene loader functionality for Eradiate that allows for the loading of `scenario.json` files to define complex scenes, including surface descriptions and canopy configurations.
    - Introduced support for preconfigured RAMI-V scenarios, leveraging the new scene loader functionality.

--- src/eradiate/scenes/biosphere/__init__.pyi
+++ src/eradiate/scenes/biosphere/__init__.pyi
@@ -1,4 +1,12 @@
 from ._canopies import wellington_citrus_orchard as wellington_citrus_orchard
+from ._canopy_loader import load_scenario as load_scenario
+from ._rami_scenarios import (
+    RAMIActualCanopies as RAMIActualCanopies,
+    RAMIHeterogeneousAbstractCanopies as RAMIHeterogeneousAbstractCanopies,
+    RAMIHomogeneousAbstractCanopies as RAMIHomogeneousAbstractCanopies,
+    RAMIScenarioVersion as RAMIScenarioVersion,
+    load_rami_scenario as load_rami_scenario,
+)
 from ._core import Canopy as Canopy
 from ._core import CanopyElement as CanopyElement
 from ._core import InstancedCanopyElement as InstancedCanopyElement
