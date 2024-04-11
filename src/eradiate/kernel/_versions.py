7dd904f468e940772c360869e1edab214964000a
Vincent Leroy <vincent.leroy@rayference.eu>
2024-04-11 17:46:08 +0200

    Update mitsuba submodule to v0.2.1

--- src/eradiate/kernel/_versions.py
+++ src/eradiate/kernel/_versions.py
@@ -6,7 +6,7 @@ from importlib.metadata import PackageNotFoundError, version
 
 # Internal constants
 REQUIRED_MITSUBA_VERSION = "3.4.1"
-REQUIRED_MITSUBA_PATCH_VERSION = "0.2.0"
+REQUIRED_MITSUBA_PATCH_VERSION = "0.2.1"
 
 
 def find_drjit():
