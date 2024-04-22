90544a06a892cceb35f3b2568c850f1a94e981f9
Vincent Leroy <vincent.leroy@rayference.eu>
2024-04-22 17:24:29 +0200

    Docs: Move add data guides to a dedicated section

--- docs/generate_rst_data.py
+++ docs/generate_rst_data.py
@@ -90,49 +90,48 @@ def generate_particle_radprops_summary():
     sections = [
         dedent(
             """
-            Aerosol / particle single-scattering radiative properties
-            =========================================================
+Aerosol / particle single-scattering radiative properties
+=========================================================
 
-            A particle radiative single-scattering property dataset provides
-            collision coefficients and scattering phase matrix data for a
-            given particle type. Particle radiative property data sets required 
-            by Eradiate are managed by the data store (see 
-            :ref:`sec-user_guide-data-intro` for details).
+A particle radiative single-scattering property dataset provides collision 
+coefficients and scattering phase matrix data for a given particle type. 
+Eradiate's built-in particle radiative property datasets are managed by the 
+data store (see :ref:`sec-data-intro` for details).
 
-            Format
-            ------
+Format
+------
 
-            * **Format** ``xarray.Dataset`` (in-memory), NetCDF (storage)
-            * **Dimensions**
+* **Format** ``xarray.Dataset`` (in-memory), NetCDF (storage)
+* **Dimensions**
 
-              * ``w``: radiation wavelength
-              * ``mu``: scattering angle cosine
-              * ``i``: scattering phase matrix row index
-              * ``j``: scattering phase matrix column index
-        
-            * **Coordinates** (all dimension coordinates; when relevant, ``units`` 
-              are required and specified in the units metadata field)
-            
-              * ``w`` float [length]
-              * ``mu`` float [dimensionless]
-              * ``i``,  ``j`` int
-            
-            * **Data variables** (when relevant, units are required and 
-              specified in the units metadata field)
-            
-              * ``sigma_t`` (``w``): volume extinction coefficient [length^-1]
-              * ``albedo`` (``w``): single-scattering albedo [dimensionless]
-              * ``phase`` (``w``, ``mu``, ``i``, ``j``): scattering phase matrix 
-                [steradian^-1]
-            
-            * **Conventions**
-            
-              * Phase matrix components use C-style indexing (from 0).
-            
-            .. dropdown:: Full validation schema
-            
-               .. literalinclude:: /resources/data_schemas/particle_dataset_v1.yml
-            """
+  * ``w``: radiation wavelength
+  * ``mu``: scattering angle cosine
+  * ``i``: scattering phase matrix row index
+  * ``j``: scattering phase matrix column index
+
+* **Coordinates** (all dimension coordinates; when relevant, ``units`` are 
+  required and specified in the units metadata field)
+
+  * ``w`` float [length]
+  * ``mu`` float [dimensionless]
+  * ``i``,  ``j`` int
+
+* **Data variables** (when relevant, units are required and  specified in the 
+  units metadata field)
+
+  * ``sigma_t`` (``w``): volume extinction coefficient [length^-1]
+  * ``albedo`` (``w``): single-scattering albedo [dimensionless]
+  * ``phase`` (``w``, ``mu``, ``i``, ``j``): scattering phase matrix 
+    [steradian^-1]
+
+* **Conventions**
+
+  * Phase matrix components use C-style indexing (from 0).
+
+.. dropdown:: Full validation schema
+
+   .. literalinclude:: /resources/data_schemas/particle_dataset_v1.yml
+"""
         ).strip()
     ]
 
