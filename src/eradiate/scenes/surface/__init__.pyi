d256b117a178c99b9874d701cbde839b3ce82986
Vincent Leroy <vincent.leroy@rayference.eu>
2024-08-01 16:56:14 +0200

    surface: Refactor all DEM components
    This commit refactors the DEM infrastructure to improve behaviour clarity
    
    * DEM-to-mesh conversion is refactored to break down the conversion
      algorithm into 4 cases with clearly specified input and output. Two
      of the four cases are not recommended for usage but included for
      convenience (mostly debugging).
    
    * The `DEMSurface` class is adapted to homogenize the logic and deduplicate
      some code.
    
    * Unit tests are updated.

--- src/eradiate/scenes/surface/__init__.pyi
+++ src/eradiate/scenes/surface/__init__.pyi
@@ -4,3 +4,4 @@ from ._core import Surface as Surface
 from ._core import surface_factory as surface_factory
 from ._dem import DEMSurface as DEMSurface
 from ._dem import mesh_from_dem as mesh_from_dem
+from ._dem import triangulate_grid as triangulate_grid
