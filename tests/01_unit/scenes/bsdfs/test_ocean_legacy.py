6818eae2ae67f66e76b988cb27d967b703460765
nicolae marton <nicolae.marton@gmail.com>
2024-10-21 14:52:11 +0200

    Add new ocean legacy BSDF plugin, based on the 6SV ocean BSDF. Update documentation accordingly.

---
+++ tests/01_unit/scenes/bsdfs/test_ocean_legacy.py
@@ -0,0 +1,35 @@
+import mitsuba as mi
+import pytest
+
+from eradiate.scenes.bsdfs import OceanLegacyBSDF
+from eradiate.test_tools.types import check_scene_element
+
+
+@pytest.mark.parametrize(
+    "kwargs",
+    [
+        {},
+        {
+            "wind_speed": 1.0,
+            "wind_direction": 15.0,
+            "chlorinity": 19.0,
+            "pigmentation": 0.3,
+        },
+        {"chlorinity": 19.0, "pigmentation": 0.3},
+    ],
+    ids=["noargs", "uniform", "mixed"],
+)
+def test_ocean_legacy_construct(modes_all, kwargs):
+    # Default constructor
+    assert OceanLegacyBSDF(**kwargs)
+
+
+def test_ocean_legacy(modes_all_double):
+    bsdf = OceanLegacyBSDF(
+        wind_speed=15.0,
+        wind_direction=42.0,
+        chlorinity=19.0,
+        pigmentation=0.3,
+    )
+
+    check_scene_element(bsdf, mi.BSDF)
