0df7dcd2ca91646616fb0c6e4b0363b05720e417
Vincent Leroy <vincent.leroy@rayference.eu>
2025-02-24 13:48:39 +0100

    Fix test

--- tests/01_unit/experiments/test_core.py
+++ tests/01_unit/experiments/test_core.py
@@ -8,8 +8,10 @@ from eradiate.units import unit_registry as ureg
 eradiate.kernel.install_logging()
 
 
-@pytest.mark.parametrize("measures", [None, 1, [0, 1]])
-def test_run_function(modes_all_double, measures):
+@pytest.mark.parametrize(
+    "measures, expected_type", [(None, dict), (1, xr.Dataset), ([0, 1], dict)]
+)
+def test_run_function(modes_all_double, measures, expected_type):
     srf = {"type": "delta", "wavelengths": [540, 550] * ureg.nm}
 
     exp = AtmosphereExperiment(
@@ -18,5 +20,5 @@ def test_run_function(modes_all_double, measures):
             {"type": "mdistant", "id": f"mdistant_{i}", "srf": srf} for i in range(3)
         ],
     )
-    result = eradiate.run(exp)
-    assert isinstance(result, xr.Dataset if measures == 1 else dict)
+    result = eradiate.run(exp, measures=measures)
+    assert isinstance(result, expected_type)
