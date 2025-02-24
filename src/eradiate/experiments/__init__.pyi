837e71dffbc05f20b63686f417fe903720133b71
Vincent Leroy <vincent.leroy@rayference.eu>
2025-02-24 14:55:44 +0100

    Experiment: Allow passing measures by ID when calling run(), postprocess() or process()

--- src/eradiate/experiments/__init__.pyi
+++ src/eradiate/experiments/__init__.pyi
@@ -3,5 +3,6 @@ from ._canopy import CanopyExperiment as CanopyExperiment
 from ._canopy_atmosphere import CanopyAtmosphereExperiment as CanopyAtmosphereExperiment
 from ._core import EarthObservationExperiment as EarthObservationExperiment
 from ._core import Experiment as Experiment
+from ._core import MeasureRegistry as MeasureRegistry
 from ._core import run as run
 from ._dem import DEMExperiment as DEMExperiment
