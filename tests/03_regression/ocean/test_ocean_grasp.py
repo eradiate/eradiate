8bb3823368d08791e6aa4eb3297459b0acd2ee9e
nicolae marton <nicolae.marton@gmail.com>
2025-03-21 10:00:02 +0100

    Add unit and regression tests to ocean_grasp and 3DREAMS scenario.

---
+++ tests/03_regression/ocean/test_ocean_grasp.py
@@ -0,0 +1,122 @@
+import pytest
+
+import eradiate
+import eradiate.data
+from eradiate.test_tools.regression import Chi2Test
+from eradiate.test_tools.test_cases.ocean import (
+    create_ocean_grasp_coastal_no_atm,
+    create_ocean_grasp_open_atm,
+    create_ocean_grasp_open_no_atm,
+    ocean_grasp_wavelength,
+)
+
+
+@pytest.mark.regression
+def test_ocean_grasp_coastal_no_atm(mode_mono_double, artefact_dir, session_timestamp):
+    """
+    Ocean GRASP Coastal no atm regression test
+    ==========================================
+
+    This is a regression test which compares the simulation results of the current
+    branch to results directly validated against the GRASP model. This test
+    target a coastal ocean scenario and doed not include an atmosphere.
+
+    Expected behaviour
+    ------------------
+
+    This test uses the Chi-squared criterion with a threshold of 0.05.
+
+    """
+    ref = eradiate.data.load_dataset(
+        "tests/regression_test_references/ocean_grasp_REF_OC_NN00_I_S20_PPL.nc"
+    )
+    exp = create_ocean_grasp_coastal_no_atm()
+    result = eradiate.run(exp)
+
+    wavelength = ocean_grasp_wavelength()
+    for w in wavelength:
+        test = Chi2Test(
+            name=(
+                f"{session_timestamp:%Y%m%d-%H%M%S}-ocean_grasp_REF_OC_NN00_I_S20_PPL"
+            ),
+            value=result.sel(w=w),
+            reference=ref.sel(w=w),
+            threshold=0.05,
+            archive_dir=artefact_dir,
+        )
+
+        assert test.run()
+
+
+@pytest.mark.regression
+def test_ocean_grasp_open_no_atm(mode_mono_double, artefact_dir, session_timestamp):
+    """
+    Ocean GRASP Open no atm regression test
+    =======================================
+
+    This is a regression test which compares the simulation results of the current
+    branch to results directly validated against the GRASP model. This test
+    target an open ocean scenario and doed not include an atmosphere.
+
+    Expected behaviour
+    ------------------
+
+    This test uses the Chi-squared criterion with a threshold of 0.05.
+
+    """
+    ref = eradiate.data.load_dataset(
+        "tests/regression_test_references/ocean_grasp_REF_OO_NN00_I_S20_PPL.nc"
+    )
+    exp = create_ocean_grasp_open_no_atm()
+    result = eradiate.run(exp)
+
+    wavelength = ocean_grasp_wavelength()
+    for w in wavelength:
+        test = Chi2Test(
+            name=(
+                f"{session_timestamp:%Y%m%d-%H%M%S}-ocean_grasp_REF_OO_NN00_I_S20_PPL"
+            ),
+            value=result.sel(w=w),
+            reference=ref.sel(w=w),
+            threshold=0.05,
+            archive_dir=artefact_dir,
+        )
+
+        assert test.run()
+
+
+@pytest.mark.regression
+def test_ocean_grasp_open_atm(mode_mono_double, artefact_dir, session_timestamp):
+    """
+    Ocean GRASP Open atm regression test
+    ====================================
+
+    This is a regression test which compares the simulation results of the current
+    branch to results directly validated against the GRASP model. This test
+    target a coastal ocean scenario and doed not include an atmosphere.
+
+    Expected behaviour
+    ------------------
+
+    This test uses the Chi-squared criterion with a threshold of 0.05.
+
+    """
+    ref = eradiate.data.load_dataset(
+        "tests/regression_test_references/ocean_grasp_REF_OO_UB01_I_S20_PPL.nc"
+    )
+    exp = create_ocean_grasp_open_atm()
+    result = eradiate.run(exp)
+
+    wavelength = ocean_grasp_wavelength()
+    for w in wavelength:
+        test = Chi2Test(
+            name=(
+                f"{session_timestamp:%Y%m%d-%H%M%S}-ocean_grasp_REF_OO_UB01_I_S20_PPL"
+            ),
+            value=result.sel(w=w),
+            reference=ref.sel(w=w),
+            threshold=0.05,
+            archive_dir=artefact_dir,
+        )
+
+        assert test.run()
