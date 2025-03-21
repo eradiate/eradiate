21275af1d511e5039d3c5a9beb3da3a2ba0a1595
nicolae marton <nicolae.marton@gmail.com>
2025-03-21 10:21:43 +0100

    Remove shininess from ocean_legacy and ocean_mishchenko.

--- tests/01_unit/scenes/bsdfs/test_ocean_legacy.py
+++ tests/01_unit/scenes/bsdfs/test_ocean_legacy.py
@@ -14,10 +14,11 @@ from eradiate.test_tools.types import check_scene_element
             "wind_direction": 15.0,
             "chlorinity": 19.0,
             "pigmentation": 0.3,
+            "shadowing": True,
         },
         {"chlorinity": 19.0, "pigmentation": 0.3},
     ],
-    ids=["noargs", "uniform", "mixed"],
+    ids=["noargs", "allargs", "mixedargs"],
 )
 def test_ocean_legacy_construct(modes_all, kwargs):
     # Default constructor
