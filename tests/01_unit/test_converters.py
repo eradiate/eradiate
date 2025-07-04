c7df29cd9e56ea55f7648e9175af7027ba77e7f3
Vincent Leroy <vincent.leroy@rayference.eu>
2025-07-04 15:47:42 +0200

    converters: Add tests for resolve_keyword()

--- tests/01_unit/test_converters.py
+++ tests/01_unit/test_converters.py
@@ -12,3 +12,10 @@ def test_passthrough():
     c = converters.passthrough_type(str)(converter)
     assert c(1.0) == 1
     assert c("1") == "1"
+
+
+def test_resolve_keyword():
+    c = converters.resolve_keyword(lambda x: f"tests/thermoprops/{x}.nc")
+    path = c("cams_lybia4_2005-04-01")
+    assert path.is_absolute()
+    assert path.is_file()
