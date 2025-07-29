ebefd21d9a3992842874bd0a79ede9c69adf3740
Vincent Leroy <vincent.leroy@rayference.eu>
2025-07-29 17:35:56 +0200

    data: Add libRadtran aerosol importer
    Co-authored-by: Nicolae Marton <nicolae.marton@gmail.com>

--- src/eradiate/data/__init__.pyi
+++ src/eradiate/data/__init__.pyi
@@ -1,3 +1,4 @@
+from . import io as io
 from ._asset_manager import AssetManager as AssetManager
 from ._asset_manager import asset_manager as asset_manager
 from ._file_resolver import FileResolver as FileResolver
