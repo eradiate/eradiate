6eea82aad9b3c6aa479cf90d1619f5cd97e51740
nicolae marton <nicolae.marton@gmail.com>
2025-08-29 14:27:54 +0200

    update directional periodic emitter to take pbox_min and pbox_max instead of periodic box.

--- src/eradiate/scenes/illumination/_directional_periodic.py
+++ src/eradiate/scenes/illumination/_directional_periodic.py
@@ -5,10 +5,11 @@ import pint
 import pinttrs
 
 from ._core import AbstractDirectionalIllumination
-from ..core import BoundingBox, NodeSceneElement, Ref
+from ..core import NodeSceneElement
 from ...attrs import define, documented
 from ...units import unit_context_kernel as ucc
 from ...units import unit_context_kernel as uck
+from ...validators import is_vector3, on_quantity
 
 
 @define(eq=False, slots=False)
@@ -29,7 +30,7 @@ class DirectionalPeriodicIllumination(AbstractDirectionalIllumination):
         pinttrs.field(
             default=None,
             validator=attrs.validators.optional(
-                pinttrs.validators.has_compatible_units
+                (pinttrs.validators.has_compatible_units, on_quantity(is_vector3))
             ),
             units=ucc.deferred("length"),
         ),
@@ -45,7 +46,7 @@ class DirectionalPeriodicIllumination(AbstractDirectionalIllumination):
         pinttrs.field(
             default=None,
             validator=attrs.validators.optional(
-                pinttrs.validators.has_compatible_units
+                (pinttrs.validators.has_compatible_units, on_quantity(is_vector3))
             ),
             units=ucc.deferred("length"),
         ),
@@ -56,40 +57,6 @@ class DirectionalPeriodicIllumination(AbstractDirectionalIllumination):
         init_type="quantity or array-like, optional",
         default="None",
     )
-
-    periodic_box: BoundingBox | Ref | None = documented(
-        attrs.field(
-            default=None,
-            validator=attrs.validators.optional(
-                attrs.validators.instance_of((BoundingBox, Ref))
-            ),
-            # converter=attrs.converters.optional(BoundingBox | Ref),
-        ),
-        doc="Bounding box defining the periodic boundary. Exclusive with "
-        "`pbox_min` and `pbox_max`.",
-        type=".BoundingBox or None",
-        init_type=".BoundingBox or dict, optional",
-        default="None",
-    )
-
-    @pbox_min.validator
-    @pbox_max.validator
-    def _pbox_validator(self, attribute, value):
-        if self.pbox_min is not None and self.pbox_max is not None:
-            if self.periodic_box is not None:
-                raise ValueError(
-                    "Cannot specify both 'pbox_min'/'pbox_max' and 'periodic_box' "
-                    "at the same time."
-                )
-
-    @periodic_box.validator
-    def _periodic_box_validator(self, attribute, value):
-        if value is not None:
-            if self.pbox_min is not None or self.pbox_max is not None:
-                raise ValueError(
-                    "Cannot specify both 'periodic_box' and 'pbox_min'/'pbox_max' "
-                    "at the same time."
-                )
 
     # --------------------------------------------------------------------------
     #                       Kernel dictionary generation
@@ -105,11 +72,6 @@ class DirectionalPeriodicIllumination(AbstractDirectionalIllumination):
         if self.pbox_min is not None and self.pbox_max is not None:
             result["pbox_min"] = self.pbox_min.m_as(uck.get("length"))
             result["pbox_max"] = self.pbox_max.m_as(uck.get("length"))
-
-        elif self.periodic_box is not None:
-            if isinstance(self.periodic_box, BoundingBox):
-                result["pbox_min"] = self.periodic_box.min.m_as(uck.get("length"))
-                result["pbox_max"] = self.periodic_box.max.m_as(uck.get("length"))
         else:
             raise ValueError(
                 "Either 'pbox_min'/'pbox_max' or 'periodic_box' must be specified."
@@ -120,7 +82,4 @@ class DirectionalPeriodicIllumination(AbstractDirectionalIllumination):
     @property
     def objects(self) -> dict[str, NodeSceneElement]:
         result = {"irradiance": self.irradiance}
-        if self.periodic_box is not None and isinstance(self.periodic_box, Ref):
-            result["periodic_box"] = self.periodic_box
-
         return result
