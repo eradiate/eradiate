54153802717798edbde6599eaf42aac5222c9a28
Vincent Leroy <vincent.leroy@rayference.eu>
2025-09-05 15:25:33 +0200

    cli: Deprecate show in favour of sys-info, add --version flag

--- src/eradiate/cli/__init__.py
+++ src/eradiate/cli/__init__.py
@@ -9,7 +9,7 @@ import typer
 from rich.logging import RichHandler
 from typing_extensions import Annotated
 
-from . import data, show, srf
+from . import data, srf, sys_info
 
 
 class LogLevel(str, Enum):
@@ -27,18 +27,37 @@ app = typer.Typer(
 )
 
 
-@app.callback()
+@app.callback(invoke_without_command=True)
 def cli(
+    ctx: typer.Context,
     log_level: Annotated[
         LogLevel, typer.Option(help="Set log level.")
     ] = LogLevel.WARNING,
     debug: Annotated[
         bool,
         typer.Option(
-            help="Enable debug mode. This will notably print exceptions with locals."
+            "--debug",
+            help="Enable debug mode. This will notably print exceptions with locals.",
+        ),
+    ] = False,
+    version: Annotated[
+        bool,
+        typer.Option(
+            "--version",
+            help="Display version information and exit.",
         ),
     ] = False,
 ):
+    if version:
+        from eradiate import __version__
+
+        print(f"eradiate version {__version__}")
+        exit(0)
+
+    if ctx.invoked_subcommand is None:
+        print(ctx.get_help())
+        ctx.exit()
+
     if debug:
         app.pretty_exceptions_enable = True
 
@@ -50,7 +69,8 @@ def cli(
     )
 
 
-app.command(name="show", help=show.__doc__)(show.main)
+app.command(name="sys-info", help=sys_info.__doc__)(sys_info.main)
+app.command(name="show", help="Alias to 'sys-info' (deprecated).")(sys_info.main)
 app.add_typer(data.app, name="data")
 app.add_typer(srf.app, name="srf")
 
