b5401d9cfd77f21f7012a68aa86db2ec0075eb96
Nicolas Misk <nicolas.misk@gmail.com>
2026-01-28 15:08:45 +0000

    Refactor the RAMI4ATM test suite, implement a pipe structure

--- src/eradiate/test_tools/test_cases/rami4atm.py
+++ src/eradiate/test_tools/test_cases/rami4atm.py
@@ -1,58 +1,24 @@
 import functools
 
+import attr
 import numpy as np
+import xarray as xr
 
+from ..regression import RMSETest, ZTest
 from ...experiments import CanopyAtmosphereExperiment
 from ...units import unit_registry as ureg
 
 
-def create_rami4atm_hom00_bla_a00s_m04_z30a000_brfpp(spp=1000):
-    r"""
-    *RAMI4ATM HOM00_BLA_S00S_M04*
-
-    This scenario is based on the ``HOM00_BLA_S00S_M04_z30a000-brfpp`` scenario
-    of the RAMI4ATM benchmark.
-
-    *Scene setup*
-
-    - Geometry: 1D plane-parallel
-    - Atmosphere: Molecular atmosphere using the AFGL 1986 (U.S. Standard) profile, absorption only
-    - Surface: Black
-    - Illumination: Directional illumination with a zenith angle of 30°
-    - Sensor: Multi-distant measure covering the principal plane, from -75° to 75° with 2° increments, delta SRF positioned at λ = 660 nm
-    """
-    # TODO: This test case definition is kept for compatibility
-    return create_rami4atm("hom00_bla_a00s_m04_z30a000_brfpp", spp)
-
-
-def create_rami4atm_hom00_bla_sd2s_m03_z30a000_brfpp(spp=1000):
-    r"""
-    *RAMI4ATM HOM00_BLA_SD2S_M03*
-
-    This scenario is based on the ``HOM00_BLA_SD2S_M03_z30a000-brfpp`` scenario
-    of the RAMI4ATM benchmark.
-
-    *Scene setup*
-
-    - Geometry: 1D plane-parallel
-    - Atmosphere: Molecular atmosphere using the AFGL 1986 (U.S. Standard) profile
-    - Aerosol layer: Uniform layer ranging from 0 km to 2 km, with AOT at 550 nm = 0.5; aerosol dataset ``govaerts_2021-desert``
-    - Surface: Black
-    - Illumination: Directional illumination with a zenith angle of 30°
-    - Sensor: Multi-distant measure covering the principal plane, from -75° to 75° with 2° increments, delta SRF positioned at λ = 550 nm
-    """
-    # TODO: This test case definition is kept for compatibility
-    return create_rami4atm("hom00_bla_sd2s_m03_z30a000_brfpp", spp)
-
-
-def create_rami4atm(id: str, spp: int):
+def create_rami4atm_toa(
+    case: str, spp: int, padding: int = 5
+) -> tuple[str, list[CanopyAtmosphereExperiment]]:
     """
     Create an :class:`.Experiment` to simulate one of the RAMI4ATM benchmarking
     cases.
 
     Parameters
     ----------
-    id : str
+    case : str
         Identifier of the test case (see notes for supported values).
 
     spp : int
@@ -60,12 +26,14 @@ def create_rami4atm(id: str, spp: int):
 
     Returns
     -------
-    .Experiment
+    tuple
+        A tuple containing the srf identifier and a list of
+        :class:`.Experiment` objects.
 
     Raises
     ------
     ValueError
-        If the passed ID contains unhandled parameter values.
+        If the passed case ID contains unhandled parameter values.
 
     Notes
     -----
@@ -104,11 +72,12 @@ def create_rami4atm(id: str, spp: int):
         * - ``measure_id``
           - ``brfpp`` (TOA BRF in the principal plane)
     """
-    canopy_id, surf_id, atm_id, band_id, illumination_id, measure_id = id.split("_")
+    canopy_id, surf_id, atm_id, band_id, illumination_id, measure_id = case.split("_")
 
     # Canopy setup
     if canopy_id == "hom00":
         canopy = None
+        target = [0.0, 0.0, 0.0]
     elif canopy_id == "hom45":
         canopy = {
             "type": "discrete_canopy",
@@ -121,12 +90,20 @@ def create_rami4atm(id: str, spp: int):
             "mu": 1.0,
             "leaf_reflectance": 0.05653,
             "leaf_transmittance": 0.01692,
-            "padding": 20,
+            "padding": padding,
+        }
+        target = {
+            "type": "rectangle",
+            "xmin": -2.5 * ureg.m,
+            "xmax": 2.5 * ureg.m,
+            "ymin": -2.5 * ureg.m,
+            "ymax": 2.5 * ureg.m,
+            "z": 2.0 * ureg.m,
         }
     else:
         raise ValueError(f"Unhandled canopy id '{canopy_id}'")
 
-    # Atmosphere setup
+    # Surface setup
     if surf_id == "bla":
         surface = {
             "type": "lambertian",
@@ -228,6 +205,15 @@ def create_rami4atm(id: str, spp: int):
         "particle_layers": [particle_layer] if particle_layer else [],
     }
 
+    srf_id = {
+        "m02": "sentinel_2a-msi-2",
+        "m03": "sentinel_2a-msi-3",
+        "m04": "sentinel_2a-msi-4",
+        "m8a": "sentinel_2a-msi-8a",
+        "m11": "sentinel_2a-msi-11",
+        "m12": "sentinel_2a-msi-12",
+    }[band_id]
+
     # Final configuration
     config = {
         "canopy": canopy,
@@ -244,22 +230,169 @@ def create_rami4atm(id: str, spp: int):
                 "construct": "hplane",
                 "zeniths": np.arange(-75, 76, 2) * ureg.deg,
                 "azimuth": 0.0 * ureg.deg,
-                "srf": "sentinel_2a-msi-4",
+                "srf": srf_id,
                 "spp": spp,
+                "target": target,
             }
         ],
         "ckd_quad_config": {"type": "gauss_legendre", "ng_max": 16, "policy": "fixed"},
         "integrator": {"type": "piecewise_volpath", "moment": True},
     }
 
-    return CanopyAtmosphereExperiment(**config)
+    return srf_id, [CanopyAtmosphereExperiment(**config)]
+
+
+def create_rami4atm_boa(
+    toa_case: str, spp: int
+) -> tuple[str, list[CanopyAtmosphereExperiment]]:
+    """
+    Create BOA experiments from Rami4ATM case id
+
+    Return 4 experiments for each BOA case, computing observed the upwelling
+    radiance and radiosity for the target, and ones that would have been
+    reflected by a perfectly diffuse surface.
+    """
+
+    ctor = registry[toa_case]["constructor"]
+
+    srf_id, (exp0,) = ctor(spp=spp)
+
+    if "hom00" in toa_case:
+        extra_objects = {
+            "boa_white_reference_patch": {
+                "factory": "shape",
+                "type": "rectangle",
+                "center": [0, 0, 0.01],
+                "edges": [1, 1],
+                "bsdf": {"type": "lambertian", "reflectance": 1.0},
+            }
+        }
+        target = [0, 0, 0.01]
+    else:
+        extra_objects = {
+            "boa_white_reference_patch": {
+                "factory": "shape",
+                "type": "rectangle",
+                "center": [0, 0, 2.025],
+                "edges": [5, 5],
+                "bsdf": {"type": "lambertian", "reflectance": 1.0},
+            }
+        }
+        target = {
+            "type": "rectangle",
+            "xmin": -2.5,
+            "xmax": 2.5,
+            "ymin": -2.5,
+            "ymax": 2.5,
+            "z": 2.025,
+        }
+
+    exp1 = attr.evolve(
+        exp0,
+        measures=[
+            {
+                "type": "mdistant",
+                "spp": spp,
+                "ray_offset": 0.05,
+                "srf": srf_id,
+                "construct": "hplane",
+                "zeniths": np.arange(-75, 76, 1),
+                "zeniths_units": "degree",
+                "azimuth": 0.0,
+                "azimuth_units": "degree",
+                "target": target,
+            }
+        ],
+    )
+    exp2 = attr.evolve(exp1, extra_objects=extra_objects)
+    dflux = {
+        "type": "distantflux",
+        "ray_offset": 0.05,
+        "target": target,
+        "srf": srf_id,
+        "spp": spp,
+    }
+    exp3 = attr.evolve(exp1, measures=[dflux])
+    exp4 = attr.evolve(exp2, measures=[dflux])
+
+    return srf_id, [exp1, exp2, exp3, exp4]
+
+
+def _calculate_boa_reflectance_factor_var(
+    da1: xr.Dataset,
+    var1: xr.Dataset,
+    da2: xr.Dataset,
+    var2: xr.Dataset,
+    srf: xr.Dataset,
+) -> xr.Dataset:
+    # Handle numerical variance estimates, assuming:
+    #  - Gaussian nature
+    #  - Null covariance of different experiments or pixels
+    #  - Local linearity of radiance and radiosity
+    #  - Scalar SRF
+
+    srf_weight = srf.srf.interp(w=da1.w.values).fillna(1e-18)
+    srf_sum = srf_weight.sum()
+
+    S1 = da1.sum() / srf_sum
+    S2 = da2.sum() / srf_sum
+    v_S1 = (var1 * srf_weight**2).sum(dim="w") / (srf_weight**2).sum()
+    v_S2 = (var2 * srf_weight**2).sum(dim="w") / (srf_weight**2).sum()
+
+    d_R_d_S1 = 1 / S2
+    d_R_d_S2 = -S1 / S2**2
+
+    v_R = d_R_d_S1**2 * v_S1 + d_R_d_S2**2 * v_S2
+
+    return v_R
+
+
+def postprocess_boa_cases(results: list[xr.Dataset], srf: xr.Dataset) -> xr.Dataset:
+    result1, result2, result3, result4 = results
+
+    result_hdrf = result1.radiance_srf / result2.radiance_srf
+    result_bhr = result3.radiosity_srf / result4.radiosity_srf
+
+    # Combine a reference dataset
+    result = result1.rename(
+        {"radiance_srf": "radiance_srf1", "radiance_var": "radiance_var1"}
+    )
+    result["radiance_srf2"] = result2.radiance_srf
+    result["radiance_var2"] = result2.radiance_var
+    result["radiosity_srf3"] = result3.radiosity_srf
+    result["radiosity_var3"] = result3.sector_radiosity_var
+    result["radiosity_srf4"] = result4.radiosity_srf
+    result["radiosity_var4"] = result4.sector_radiosity_var
+    result["hdrf"] = result_hdrf
+    result["bhr"] = result_bhr
+
+    result["hdrf_var"] = _calculate_boa_reflectance_factor_var(
+        result1.radiance,
+        result1.radiance_var,
+        result2.radiance,
+        result2.radiance_var,
+        srf,
+    )
+    result["bhr_var"] = _calculate_boa_reflectance_factor_var(
+        result3.radiosity,
+        result3.sector_radiosity_var,
+        result4.radiosity,
+        result4.sector_radiosity_var,
+        srf,
+    )
+
+    return result
 
 
 #: Mapping of test case to a generation function for all RAMI4ATM test cases
 #: available in the Eradiate regression test suite.
+
 registry = {
-    x: functools.partial(create_rami4atm, id=x)
-    for x in [
+    case: {
+        "constructor": functools.partial(create_rami4atm_toa, case=case),
+        "threshold": 0.005,
+    }
+    for case in [
         "hom00_whi_s00s_m04_z30a000_brfpp",
         "hom00_bla_a00s_m04_z30a000_brfpp",
         "hom00_rpv_e00s_m04_z30a000_brfpp",
@@ -283,4 +416,34 @@ registry = {
         "hom00_rpv_ed6s_m04_z30a000_brfpp",
         "hom45_lam_ec2s_m04_z30a000_brfpp",
     ]
+}
+
+
+registry["hom00_bla_sd2s_m03_z30a000_brfpp"] = {
+    "constructor": functools.partial(
+        create_rami4atm_toa, case="hom00_bla_sd2s_m03_z30a000_brfpp"
+    ),
+    "test": ZTest,
+    "threshold": 0.05,
+    "variables": ["radiance"],
+}
+
+registry["hom00_whi_s00s_m04_z30a000_boa"] = {
+    "constructor": functools.partial(
+        create_rami4atm_boa, toa_case="hom00_whi_s00s_m04_z30a000_brfpp"
+    ),
+    "test": RMSETest,
+    "threshold": 2e-3,
+    "variables": ["hdrf", "bhr"],
+    "postprocess": postprocess_boa_cases,
+}
+
+registry["hom00_rpv_e00s_m04_z30a000_boa"] = {
+    "constructor": functools.partial(
+        create_rami4atm_boa, toa_case="hom00_rpv_e00s_m04_z30a000_brfpp"
+    ),
+    "test": RMSETest,
+    "threshold": 2e-5,
+    "variables": ["hdrf", "bhr"],
+    "postprocess": postprocess_boa_cases,
 }
