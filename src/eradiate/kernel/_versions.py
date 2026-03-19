a7c5798b80603110f3507acf2286df24a28f19c6
Vincent Leroy <vincent.leroy@rayference.eu>
2026-03-19 09:11:44 +0100

    Reqs: Bump required kernel version and update lock file

--- src/eradiate/kernel/_versions.py
+++ src/eradiate/kernel/_versions.py
@@ -6,7 +6,7 @@ from importlib.metadata import PackageNotFoundError, version
 
 # Internal constants
 REQUIRED_MITSUBA_VERSION = "3.7.1"
-REQUIRED_MITSUBA_PATCH_VERSION = "0.4.2"
+REQUIRED_MITSUBA_PATCH_VERSION = "0.4.3"
 
 
 def find_drjit():
